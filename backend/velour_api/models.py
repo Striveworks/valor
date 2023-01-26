d5e859004666b0eb6415dbb78319c9748095315a
Eric O. Korman <eric.korman@gmail.com>
2023-01-26 16:24:52 -0600

    schema update

--- backend/velour_api/models.py
+++ backend/velour_api/models.py
@@ -1,5 +1,6 @@
 from geoalchemy2 import Geometry
-from sqlalchemy import Column, Float, Integer, String
+from sqlalchemy import Column, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 from .database import Base
 
@@ -16,19 +17,65 @@ class Detection(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     boundary = Column(Geometry("POLYGON"))
+    image = Column(Integer, ForeignKey("image.id"))
+    labeled_detections = relationship("LabeledDetection")
     # should add bounding box here too?
     # can get this from ST_Envelope
     # use https://docs.sqlalchemy.org/en/14/orm/mapping_columns.html#sqlalchemy.orm.column_property
-    score = Column(Float)  # between 0 and 1, -1 if groundtruth
-    class_label = Column(String)
+    # score = Column(Float)  # between 0 and 1, -1 if groundtruth
+
+
+class LabeledDetection(Base):
+    # also used for instance segmentation
+    __tablename__ = "labeled_detection"
+
+    id = Column(Integer, primary_key=True, index=True)
+    score = Column(Float)
+    detection = Column(Integer, ForeignKey("detection.id"))
+    label = Column(Integer, ForeignKey("label.id"))
+
+
+class LabeledImage(Base):
+    __tablename__ = "labeled_image"
+
+    id = Column(Integer, primary_key=True, index=True)
+    score = Column(Float)
+    # need some uniquess for labels (a key can only appear once for a given image)
+    image = Column(Integer, foreign_key="image.id")
+    label = Column(Integer, ForeignKey("label.id"))
+
+
+class Segmentation(Base):
+    """Represents a single detection in an image"""
+
+    __tablename__ = "segmentation"
+
+    id = Column(Integer, primary_key=True, index=True)
+    boundary = Column(Geometry("POLYGON"))
+    image = Column(Integer, ForeignKey("image.id"))
+    labeled_segmentations = relationship("LabeledSegmentation")
+
+
+class LabeledSegmentation(Base):
+    # also used for instance segmentation
+    __tablename__ = "labeled_segmentation"
+
+    id = Column(Integer, primary_key=True, index=True)
+    score = Column(Float)
+    segmentation = Column(Integer, ForeignKey("segmentation.id"))
+    label = Column(Integer, ForeignKey("label.id"))
 
 
 class Image(Base):
     """Represents an image"""
 
+    # make every image belong to just one dataset but will be able to
+    # have dataset "views"
+
     __tablename__ = "image"
 
     id = Column(Integer, primary_key=True, index=True)
+    dataset = Column(Integer, foreign_key="dataset.id")
     href = Column(String)
 
 
@@ -57,6 +104,20 @@ class Dataset:
     name = Column(String)
 
 
-class DatasetImage:
-    # collection of datasets and images
-    pass
+class Label:
+    __tablename__ = "label"
+
+    id = Column(Integer, primary_key=True, index=True)
+    # (key, value) should be unique
+    key = Column(String)
+    value = Column(String)
+    labeled_detections = relationship("LabeledDetection")
+    labeled_segmentations = relationship("LabeledSegmentation")
+    labeled_images = relationship("LabeledImage")
+
+
+"""
+Queries:
+- get all detections in a dataset with a given label key
+- get all detections in a dataset with a given label key and values in a list
+"""
