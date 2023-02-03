f015cbe1d1a38a56887b936820b0c8ca975346ea
Eric O. Korman <eric.korman@gmail.com>
2023-02-03 17:03:19 -0600

    fix unit tests

--- backend/tests/unit-tests/test_schemas.py
+++ backend/tests/unit-tests/test_schemas.py
@@ -1,16 +1,26 @@
 import pytest
 
-from velour_api.schemas import GroundTruthDetectionCreate
+from velour_api.schemas import GroundTruthDetectionCreate, Image, Label
 
 
 def test_ground_truth_detection_validation_pos():
     boundary = [[1, 1], [2, 2], [0, 4]]
-    det = GroundTruthDetectionCreate(boundary=boundary, class_label="class")
+    det = GroundTruthDetectionCreate(
+        boundary=boundary,
+        class_label="class",
+        labels=[Label(key="class", value="a")],
+        image=Image(uri=""),
+    )
     assert det.boundary == [tuple(pt) for pt in boundary]
 
 
 def test_ground_truth_detection_validation_neg():
     boundary = [[1, 1], [2, 2]]
     with pytest.raises(ValueError) as exc_info:
-        GroundTruthDetectionCreate(boundary=boundary, class_label="class")
+        GroundTruthDetectionCreate(
+            boundary=boundary,
+            class_label="class",
+            labels=[Label(key="class", value="a")],
+            image=Image(uri=""),
+        )
     assert "must be composed of at least three points" in str(exc_info)
