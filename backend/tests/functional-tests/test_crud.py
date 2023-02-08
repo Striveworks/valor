d105e9c261364ee0168e45f33b983aa17deb4a38
Eric O. Korman <eric.korman@gmail.com>
2023-02-08 16:05:11 -0600

    update test

--- backend/tests/functional-tests/test_crud.py
+++ backend/tests/functional-tests/test_crud.py
@@ -4,9 +4,24 @@ from sqlalchemy.orm import Session
 
 from velour_api import crud, models, schemas
 
+dset_name = "test dataset"
+
+
+@pytest.fixture
+def gt_dets_create() -> schemas.GroundTruthDetectionsCreate:
+    return schemas.GroundTruthDetectionsCreate(
+        dataset_name=dset_name,
+        detections=[
+            schemas.DetectionBase(
+                boundary=[(10, 20), (10, 30), (20, 30), (20, 20)],
+                labels=[schemas.Label(key="k", value="v")],
+                image=schemas.Image(uri="uri1"),
+            )
+        ],
+    )
+
 
 def test_create_and_get_datasets(db: Session):
-    dset_name = "test dataset"
     crud.create_dataset(db, schemas.DatasetCreate(name=dset_name))
 
     all_datasets = db.scalars(select(models.Dataset)).all()
@@ -24,8 +39,6 @@ def test_create_and_get_datasets(db: Session):
 
 
 def test_get_dataset(db: Session):
-    dset_name = "test dataset"
-
     with pytest.raises(crud.DatasetDoesNotExistError) as exc_info:
         crud.get_dataset(db, dset_name)
     assert "does not exist" in str(exc_info)
@@ -35,8 +48,9 @@ def test_get_dataset(db: Session):
     assert dset.name == dset_name
 
 
-def test_delete_dataset(db: Session):
-    dset_name = "test dataset"
+def test_delete_dataset(
+    db: Session, gt_dets_create: schemas.GroundTruthDetectionsCreate
+):
     crud.create_dataset(db, schemas.DatasetCreate(name=dset_name))
 
     # sanity check nothing in db
@@ -46,21 +60,9 @@ def test_delete_dataset(db: Session):
         models.LabeledGroundTruthDetection,
         models.Label,
     ]:
-        assert db.scalars(select(func.count(model_cls.id))).first() == 0
+        assert db.scalar(select(func.count(model_cls.id))) == 0
 
-    crud.create_groundtruth_detections(
-        db,
-        data=schemas.GroundTruthDetectionsCreate(
-            dataset_name=dset_name,
-            detections=[
-                schemas.DetectionBase(
-                    boundary=[(10, 20), (10, 30), (20, 30), (20, 20)],
-                    labels=[schemas.Label(key="k", value="v")],
-                    image=schemas.Image(uri="uri1"),
-                )
-            ],
-        ),
-    )
+    crud.create_groundtruth_detections(db, data=gt_dets_create)
 
     # should have one row for all of these tables
     for model_cls in [
@@ -68,7 +70,7 @@ def test_delete_dataset(db: Session):
         models.GroundTruthDetection,
         models.LabeledGroundTruthDetection,
     ]:
-        assert db.scalars(select(func.count(model_cls.id))).first() == 1
+        assert db.scalar(select(func.count(model_cls.id))) == 1
 
     # delete dataset and check the cascade worked
     crud.delete_dataset(db, dataset_name=dset_name)
@@ -78,7 +80,7 @@ def test_delete_dataset(db: Session):
         models.GroundTruthDetection,
         models.LabeledGroundTruthDetection,
     ]:
-        assert db.scalars(select(func.count(model_cls.id))).first() == 0
+        assert db.scalar(select(func.count(model_cls.id))) == 0
 
     # make åsure labels are still there`
-    assert db.scalars(select(func.count(models.Label.id))).first() == 1
+    assert db.scalar(select(func.count(models.Label.id))) == 1
