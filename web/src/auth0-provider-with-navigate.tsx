419116f51ee779da5bf276832ac549c8a05bb485
Eric O. Korman <eric.korman@gmail.com>
2023-03-07 07:38:09 -0600

    more debugging

--- web/src/auth0-provider-with-navigate.tsx
+++ web/src/auth0-provider-with-navigate.tsx
@@ -1,4 +1,4 @@
-import { Auth0Provider, AppState, User } from "@auth0/auth0-react";
+import { Auth0Provider, AppState } from "@auth0/auth0-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
