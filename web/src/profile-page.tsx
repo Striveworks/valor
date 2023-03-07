791cae0e9b8c22a38d91c941c71675f3f6c1ec84
Eric O. Korman <eric.korman@gmail.com>
2023-03-06 20:11:32 -0600

    enhance profile page

--- web/src/profile-page.tsx
+++ web/src/profile-page.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { LogoutButton } from "./logout-button";
+import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 export const ProfilePage = () => {
   const [accessToken, setAccessToken] = useState("");
@@ -17,6 +19,7 @@ export const ProfilePage = () => {
   const fields = ["name", "email"];
   return (
     <div>
+      <h2>User Information</h2>
       <table>
         <tbody>
           {fields.map((f) => (
@@ -25,12 +28,18 @@ export const ProfilePage = () => {
               <td>{user ? user[f] : null}</td>
             </tr>
           ))}
-          <tr>
-            <th>Access token:</th>
-            <td style={{ overflow: "scroll" }}>{accessToken}</td>
-          </tr>
         </tbody>
       </table>
+      <h2>
+        The python snippet below establishes an authenticated connection to the
+        velour instance.
+      </h2>
+
+      <div style={{ width: "75%" }}>
+        <SyntaxHighlighter language="python" style={atomOneDark}>
+          {`from velour.client import Client\n\nclient = Client("${process.env.REACT_APP_AUTH0_AUDIENCE}", access_token="${accessToken}")`}
+        </SyntaxHighlighter>
+      </div>
       <LogoutButton />
     </div>
   );
