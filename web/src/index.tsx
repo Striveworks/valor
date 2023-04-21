1bfe3c78ca8c291040f22ea750d66c3f27b4f1ff
Eric O. Korman <eric.korman@gmail.com>
2023-04-21 12:04:11 -0500

    update theme to use react router links, SW styling

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -8,9 +8,55 @@ import { ThemeProvider } from "@mui/material/styles";
 import { createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { StyledEngineProvider } from "@mui/material/styles";
-// import AnekTamil from "@fontsource/anek-tamil";
+import {
+  Link as RouterLink,
+  LinkProps as RouterLinkProps,
+} from "react-router-dom";
+import { LinkProps } from "@mui/material/Link";
+
+const LinkBehavior = React.forwardRef<
+  HTMLAnchorElement,
+  Omit<RouterLinkProps, "to"> & { href: RouterLinkProps["to"] }
+>((props, ref) => {
+  const { href, ...other } = props;
+  // Map href (MUI) -> to (react-router)
+  return <RouterLink ref={ref} to={href} {...other} />;
+});
+
+export const StriveColors = {
+  logoPurple: "#820782",
+  logoOutline: "#25034c",
+  strongPurple: "#7209b7",
+  ctaGreen: "#36CFC9",
+  lightPurple: "#AA6CFF",
+  transitionBlue: "#3A0CA3",
+  brightBlue: "#426BF1",
+  transitionSeafoam: "#A6CEE3",
+  altButton: "#F0F0F0",
+  backgroundPrimary: "#2C3550",
+  backgroundDark: "#232D49",
+  placeholder: "#7E89A9",
+  breadCrumb: "#FCE26B",
+  error: "#E64444",
+  success: "#1CBA46",
+};
 
 const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: StriveColors.brightBlue,
+      dark: StriveColors.backgroundDark,
+    },
+    secondary: {
+      main: StriveColors.logoPurple,
+      dark: StriveColors.logoOutline,
+    },
+    background: {
+      default: StriveColors.backgroundPrimary,
+      paper: StriveColors.backgroundDark,
+    },
+  },
   typography: {
     fontFamily: "Anek Tamil",
     allVariants: {
@@ -21,7 +67,8 @@ const theme = createTheme({
     MuiCssBaseline: {
       styleOverrides: {
         body: {
-          backgroundImage: `linear-gradient(to right top, #4c4355, #3d323e, #2c2329, #1a1517, #000000)`,
+          // backgroundImage: `linear-gradient(to right top, #4c4355, #3d323e, #2c2329, #1a1517, #000000)`,
+          margin: `15px`,
         },
         html: {
           height: `100%`,
@@ -44,6 +91,16 @@ const theme = createTheme({
           verticalAlign: `top`,
           padding: `0 0px`,
         },
+      },
+    },
+    MuiLink: {
+      defaultProps: {
+        component: LinkBehavior,
+      } as LinkProps,
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        LinkComponent: LinkBehavior,
       },
     },
   },
