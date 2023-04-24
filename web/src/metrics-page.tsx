f3b59791a099922964ecb1cd6e3beca0d872acba
Eric O. Korman <eric.korman@gmail.com>
2023-04-24 10:19:33 -0500

    dynamic metric tables

--- web/src/metrics-page.tsx
+++ web/src/metrics-page.tsx
@@ -2,23 +2,41 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import FormControl from "@mui/material/FormControl";
 
 type Metric = {
   type: string;
-  parameters: object;
-  label: { key: string; value: string };
+  parameters: { iou: number; ious: number[] };
+  label?: { key: string; value: string };
   value: number;
 };
-// type MetricWithId =
 
-const columns: GridColDef[] = [
-  { field: "type", headerName: "Type" },
+type MetricAtIOU = {
+  labelKey?: string;
+  labelValue?: string;
+  value: number;
+  iou: number;
+  id: number;
+};
+
+const APColumns: GridColDef[] = [
+  { field: "labelKey", headerName: "Label Key" },
+  { field: "labelValue", headerName: "Label Value" },
+  { field: "iou", headerName: "IOU" },
   { field: "value", headerName: "Value" },
-  { field: "parameters", headerName: "Parameters" },
+];
+
+const mAPColumns: GridColDef[] = [
+  { field: "iou", headerName: "IOU" },
+  { field: "value", headerName: "Value" },
 ];
 
 export const MetricsPage = () => {
   let { name, evalSettingsId } = useParams();
+  const [selectedMetricType, setSelectedMetricType] = useState("");
   const [metrics, setMetrics] = useState<Metric[]>([]);
   const metricsWithIds = metrics.map((m, i) => ({ ...m, id: i }));
   useEffect(() => {
@@ -27,23 +45,75 @@ export const MetricsPage = () => {
     axios.get(url).then((response) => {
       setMetrics(response.data);
     });
-  }, []);
+  });
   if (!metrics) return null;
+
+  const APs: MetricAtIOU[] = [];
+  const mAPs: MetricAtIOU[] = [];
+
+  metricsWithIds.forEach((x) => {
+    if (x.type === "AP") {
+      APs.push({
+        labelKey: x.label?.key,
+        labelValue: x.label?.value,
+        value: x.value,
+        iou: x.parameters.iou,
+        id: x.id,
+      });
+    } else if (x.type === "mAP") {
+      mAPs.push({ value: x.value, iou: x.parameters.iou, id: x.id });
+    }
+  });
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setSelectedMetricType(event.target.value as string);
+    console.log(`metric selected: ${selectedMetricType}`);
+  };
+
   return (
     <>
-      <DataGrid
-        rows={metricsWithIds}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 5,
+      <FormControl fullWidth>
+        <InputLabel id="select-label">Metric</InputLabel>
+        <Select
+          labelId="select-label"
+          id="demo-simple-select"
+          value={selectedMetricType}
+          label="Metric"
+          onChange={handleChange}
+        >
+          <MenuItem value={"AP"}>AP</MenuItem>
+          <MenuItem value={"mAP"}>mAP</MenuItem>
+        </Select>
+      </FormControl>
+      {selectedMetricType === "AP" ? (
+        <DataGrid
+          rows={APs}
+          columns={APColumns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
             },
-          },
-        }}
-        pageSizeOptions={[5]}
-        disableRowSelectionOnClick
-      />
+          }}
+          pageSizeOptions={[5]}
+          disableRowSelectionOnClick
+        />
+      ) : (
+        <DataGrid
+          rows={mAPs}
+          columns={mAPColumns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          disableRowSelectionOnClick
+        />
+      )}
     </>
   );
 };
