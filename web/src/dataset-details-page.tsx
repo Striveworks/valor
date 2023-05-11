84066c4af2b228a10a2f53417f8b318e95bf5fd5
Eric O. Korman <eric.korman@gmail.com>
2023-05-11 17:22:04 -0500

    remove unused imports

--- web/src/dataset-details-page.tsx
+++ web/src/dataset-details-page.tsx
@@ -1,10 +1,3 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import Typography from "@mui/material/Typography";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import Link from "@mui/material/Link";
-import { EntityResponse, EvaluationSetting } from "./types";
 import { Wrapper } from "./components/wrapper";
 import { EntityDetailsComponent } from "./components/entity-details-component";
 
