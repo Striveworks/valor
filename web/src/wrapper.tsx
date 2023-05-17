f421b3f3eff83e75d7d695f7418e99d5667b90fb
Charles Zaloom <czaloom4@gmail.com>
2023-05-17 16:52:05 -0500

    merge

--- web/src/wrapper.tsx
+++ web/src/wrapper.tsx
@@ -1,17 +1,12 @@
 import Drawer from "@mui/material/Drawer";
-import CssBaseline from "@mui/material/CssBaseline";
-import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
 import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
 import React from "react";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 
 const drawerWidth = 240;
 
@@ -39,8 +34,15 @@ export const Wrapper = ({ children }: { children: React.ReactNode }) => (
         <ListItem>
           <Link href="/models">Models</Link>
         </ListItem>
+        <Divider />
         <ListItem>
           <Link href="/profile">Profile</Link>
+        </ListItem>
+        <Divider />
+        <ListItem>
+          <Link href="/docs/" component="a" target="_blank">
+            Docs <OpenInNewIcon />
+          </Link>
         </ListItem>
       </List>
     </Drawer>
