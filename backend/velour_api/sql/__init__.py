7b185d7d2e15d1e1fa52f873dd0ff5116ee24ce7
Charles Zaloom <czaloom4@gmail.com>
2023-06-08 17:05:16 -0500

    moved iou calculation and gt/pd ranked pairing to postgres

---
+++ backend/velour_api/sql/__init__.py
@@ -0,0 +1,12 @@
+from .query import (
+    convert_polygons_to_bbox,
+    convert_raster_to_bbox,
+    convert_raster_to_polygons,
+    join_labels,
+    join_tables,
+    compute_iou,
+    get_labels,
+    get_number_of_ground_truths,
+    get_sorted_ranked_pairs,
+    function_find_ranked_pairs,
+)
