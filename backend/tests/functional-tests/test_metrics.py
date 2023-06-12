e0ecbbcc798bbb097322b54883b72843831de582
Charles Zaloom <czaloom4@gmail.com>
2023-06-12 10:57:01 -0500

    sanity check 2

--- backend/tests/functional-tests/test_metrics.py
+++ backend/tests/functional-tests/test_metrics.py
@@ -2,16 +2,18 @@ import pytest
 from sqlalchemy.orm import Session
 
 from velour_api import crud, schemas
-from velour_api.metrics import compute_ap_metrics
+
+# from velour_api.metrics import compute_ap_metrics
 from velour_api.metrics.classification import (
     accuracy_from_cm,
     confusion_matrix_at_label_key,
     roc_auc,
 )
-from velour_api.models import (
-    LabeledGroundTruthDetection,
-    LabeledPredictedDetection,
-)
+
+# from velour_api.models import (
+#     LabeledGroundTruthDetection,
+#     LabeledPredictedDetection,
+# )
 
 dataset_name = "test dataset"
 model_name = "test model"
@@ -109,103 +111,109 @@ def round_dict_(d: dict, prec: int) -> None:
             round_dict_(v, prec)
 
 
-def test_compute_ap_metrics(
-    db: Session,
-    groundtruths: list[list[LabeledGroundTruthDetection]],
-    predictions: list[list[LabeledPredictedDetection]],
-):
+def test_running_sql(db: Session):
+    from sqlalchemy import text
 
-    model_name = "test model"
-    dataset_name = "test dataset"
+    db.execute(text("select * from test"))
 
-    dataset_id = crud.get_dataset(db, dataset_name).id
-    model_id = crud.get_model(db, model_name).id
 
-    iou_thresholds = set([round(0.5 + 0.05 * i, 2) for i in range(10)])
-    metrics = compute_ap_metrics(
-        db=db,
-        dataset_id=dataset_id,
-        model_id=model_id,
-        gt_type=schemas.Task.BBOX_OBJECT_DETECTION,
-        pd_type=schemas.Task.BBOX_OBJECT_DETECTION,
-        label_key="class",
-        iou_thresholds=iou_thresholds,
-        ious_to_keep=[0.5, 0.75],
-    )
+# def test_compute_ap_metrics(
+#     db: Session,
+#     groundtruths: list[list[LabeledGroundTruthDetection]],
+#     predictions: list[list[LabeledPredictedDetection]],
+# ):
 
-    metrics = [m.dict() for m in metrics]
-    for m in metrics:
-        round_dict_(m, 3)
+#     model_name = "test model"
+#     dataset_name = "test dataset"
 
-    # cf with torch metrics/pycocotools results listed here:
-    # https://github.com/Lightning-AI/metrics/blob/107dbfd5fb158b7ae6d76281df44bd94c836bfce/tests/unittests/detection/test_map.py#L231
-    expected = [
-        # AP METRICS
-        {"iou": 0.5, "value": 0.505, "label": {"key": "class", "value": "2"}},
-        {"iou": 0.75, "value": 0.505, "label": {"key": "class", "value": "2"}},
-        {"iou": 0.5, "value": 0.79, "label": {"key": "class", "value": "49"}},
-        {
-            "iou": 0.75,
-            "value": 0.576,
-            "label": {"key": "class", "value": "49"},
-        },
-        {"iou": 0.5, "value": -1.0, "label": {"key": "class", "value": "3"}},
-        {"iou": 0.75, "value": -1.0, "label": {"key": "class", "value": "3"}},
-        {"iou": 0.5, "value": 1.0, "label": {"key": "class", "value": "0"}},
-        {"iou": 0.75, "value": 0.723, "label": {"key": "class", "value": "0"}},
-        {"iou": 0.5, "value": 1.0, "label": {"key": "class", "value": "1"}},
-        {"iou": 0.75, "value": 1.0, "label": {"key": "class", "value": "1"}},
-        {"iou": 0.5, "value": 1.0, "label": {"key": "class", "value": "4"}},
-        {"iou": 0.75, "value": 1.0, "label": {"key": "class", "value": "4"}},
-        # mAP METRICS
-        {"iou": 0.5, "value": 0.859},
-        {"iou": 0.75, "value": 0.761},
-        # AP METRICS AVERAGED OVER IOUS
-        {
-            "ious": iou_thresholds,
-            "value": 0.454,
-            "label": {"key": "class", "value": "2"},
-        },
-        {
-            "ious": iou_thresholds,
-            "value": 0.555,  # note COCO had 0.556
-            "label": {"key": "class", "value": "49"},
-        },
-        {
-            "ious": iou_thresholds,
-            "value": -1.0,
-            "label": {"key": "class", "value": "3"},
-        },
-        {
-            "ious": iou_thresholds,
-            "value": 0.725,
-            "label": {"key": "class", "value": "0"},
-        },
-        {
-            "ious": iou_thresholds,
-            "value": 0.8,
-            "label": {"key": "class", "value": "1"},
-        },
-        {
-            "ious": iou_thresholds,
-            "value": 0.650,
-            "label": {"key": "class", "value": "4"},
-        },
-        # mAP METRICS AVERAGED OVER IOUS
-        {"ious": iou_thresholds, "value": 0.637},
-    ]
+#     dataset_id = crud.get_dataset(db, dataset_name).id
+#     model_id = crud.get_model(db, model_name).id
 
-    # sort labels lists
-    for m in metrics + expected:
-        if "labels" in m:
-            m["labels"] = sorted(m["labels"], key=lambda x: x["value"])
+#     iou_thresholds = set([round(0.5 + 0.05 * i, 2) for i in range(10)])
+#     metrics = compute_ap_metrics(
+#         db=db,
+#         dataset_id=dataset_id,
+#         model_id=model_id,
+#         gt_type=schemas.Task.BBOX_OBJECT_DETECTION,
+#         pd_type=schemas.Task.BBOX_OBJECT_DETECTION,
+#         label_key="class",
+#         iou_thresholds=iou_thresholds,
+#         ious_to_keep=[0.5, 0.75],
+#     )
 
-    # check that metrics and labels are equivalent
-    for m in metrics:
-        assert m in expected
+#     metrics = [m.dict() for m in metrics]
+#     for m in metrics:
+#         round_dict_(m, 3)
 
-    for m in expected:
-        assert m in metrics
+#     # cf with torch metrics/pycocotools results listed here:
+#     # https://github.com/Lightning-AI/metrics/blob/107dbfd5fb158b7ae6d76281df44bd94c836bfce/tests/unittests/detection/test_map.py#L231
+#     expected = [
+#         # AP METRICS
+#         {"iou": 0.5, "value": 0.505, "label": {"key": "class", "value": "2"}},
+#         {"iou": 0.75, "value": 0.505, "label": {"key": "class", "value": "2"}},
+#         {"iou": 0.5, "value": 0.79, "label": {"key": "class", "value": "49"}},
+#         {
+#             "iou": 0.75,
+#             "value": 0.576,
+#             "label": {"key": "class", "value": "49"},
+#         },
+#         {"iou": 0.5, "value": -1.0, "label": {"key": "class", "value": "3"}},
+#         {"iou": 0.75, "value": -1.0, "label": {"key": "class", "value": "3"}},
+#         {"iou": 0.5, "value": 1.0, "label": {"key": "class", "value": "0"}},
+#         {"iou": 0.75, "value": 0.723, "label": {"key": "class", "value": "0"}},
+#         {"iou": 0.5, "value": 1.0, "label": {"key": "class", "value": "1"}},
+#         {"iou": 0.75, "value": 1.0, "label": {"key": "class", "value": "1"}},
+#         {"iou": 0.5, "value": 1.0, "label": {"key": "class", "value": "4"}},
+#         {"iou": 0.75, "value": 1.0, "label": {"key": "class", "value": "4"}},
+#         # mAP METRICS
+#         {"iou": 0.5, "value": 0.859},
+#         {"iou": 0.75, "value": 0.761},
+#         # AP METRICS AVERAGED OVER IOUS
+#         {
+#             "ious": iou_thresholds,
+#             "value": 0.454,
+#             "label": {"key": "class", "value": "2"},
+#         },
+#         {
+#             "ious": iou_thresholds,
+#             "value": 0.555,  # note COCO had 0.556
+#             "label": {"key": "class", "value": "49"},
+#         },
+#         {
+#             "ious": iou_thresholds,
+#             "value": -1.0,
+#             "label": {"key": "class", "value": "3"},
+#         },
+#         {
+#             "ious": iou_thresholds,
+#             "value": 0.725,
+#             "label": {"key": "class", "value": "0"},
+#         },
+#         {
+#             "ious": iou_thresholds,
+#             "value": 0.8,
+#             "label": {"key": "class", "value": "1"},
+#         },
+#         {
+#             "ious": iou_thresholds,
+#             "value": 0.650,
+#             "label": {"key": "class", "value": "4"},
+#         },
+#         # mAP METRICS AVERAGED OVER IOUS
+#         {"ious": iou_thresholds, "value": 0.637},
+#     ]
+
+#     # sort labels lists
+#     for m in metrics + expected:
+#         if "labels" in m:
+#             m["labels"] = sorted(m["labels"], key=lambda x: x["value"])
+
+#     # check that metrics and labels are equivalent
+#     for m in metrics:
+#         assert m in expected
+
+#     for m in expected:
+#         assert m in metrics
 
 
 def test_confusion_matrix_at_label_key(db: Session, classification_test_data):
