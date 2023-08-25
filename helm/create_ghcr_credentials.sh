ab92a1a57bafec620ba02f75c0b522bc72e51cb9
Charles Zaloom <czaloom4@gmail.com>
2023-08-25 15:35:24 -0500

    working on helm chart

---
+++ helm/create_ghcr_credentials.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Prompt for GitHub username
+read -p "Enter your GitHub username: " github_username
+
+# Prompt for GitHub token
+read -sp "Enter your GitHub token: " github_token
+echo
+
+# Create Kubernetes secret for GitHub credentials
+kubectl create secret docker-registry ghcr-secret \
+    --docker-server=ghcr.io \
+    --docker-username="$github_username" \
+    --docker-password="$github_token" \
+    --docker-email=""
