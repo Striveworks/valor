e65dfe085eaf337b3c47795181f2b3289cd82628
Charles Zaloom <czaloom4@gmail.com>
2023-08-25 16:32:19 -0500

    helm chart is working

--- helm/create_ghcr_credentials.sh
+++ helm/create_ghcr_credentials.sh
@@ -8,7 +8,7 @@ read -sp "Enter your GitHub token: " github_token
 echo
 
 # Create Kubernetes secret for GitHub credentials
-kubectl create secret docker-registry ghcr-secret \
+kubectl create secret docker-registry velour-ghcr-secret \
     --docker-server=ghcr.io \
     --docker-username="$github_username" \
     --docker-password="$github_token" \
