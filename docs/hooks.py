b3503ba823de27f0fbca94362c1b58fcc6a39350
Charles Zaloom <czaloom4@gmail.com>
2023-09-08 10:15:29 -0500

    new helm chart workflow

--- docs/hooks.py
+++ docs/hooks.py
@@ -1,16 +1,12 @@
-import os
-import shutil
+# import os
+# import re
+# import shutil
 
 
-def copy_get(config, **kwargs):
-    site_dir = config["site_dir"]
-    shutil.make_archive(
-        base_name="helm/velour-chart-latest",
-        format="gztar",
-        root_dir=".",
-        base_dir="helm/velour-chart",
-    )
-    shutil.copy(
-        "helm/velour-chart-latest.tar.gz",
-        os.path.join(site_dir, "velour-chart-latest.tgz"),
-    )
+# def copy_get(config, **kwargs):
+#     site_dir = config["site_dir"]
+#     url = "https://localhost:8000"
+#     chart = config['chart']
+#     print(chart)
+#     os.system(f"helm package helm/velour-chart -d {site_dir}/helm")
+#     os.system(f"helm repo index {site_dir}/helm --url {url}")
