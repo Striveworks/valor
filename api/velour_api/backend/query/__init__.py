d5c9fa42a7996cf466bbc8e88ba1ef83c4f0ebaf
Nick <nthorlind@gmail.com>
2023-10-18 12:50:32 -0600

    delete get_labels

--- api/velour_api/backend/query/__init__.py
+++ api/velour_api/backend/query/__init__.py
@@ -12,7 +12,7 @@ from .label import (
     get_joint_keys,
     get_joint_labels,
     get_label_distribution,
-    get_labels_for_annotation,
+    get_labels,
 )
 from .metadata import get_metadata
 from .metrics import (
@@ -41,7 +41,7 @@ __all__ = [
     "create_prediction",
     "get_prediction",
     "get_predictions",
-    "get_labels_for_annotation",
+    "get_labels",
     "get_label_distribution",
     "get_scored_label_distribution",
     "get_joint_labels",
