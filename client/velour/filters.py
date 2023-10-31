a413de268118931b08ebc4528579b866ab4532f4
Charles Zaloom <czaloom4@gmail.com>
2023-10-31 14:14:18 -0500

    wip

--- client/velour/filters.py
+++ client/velour/filters.py
@@ -1,56 +1,82 @@
 class DeclarativeMapper:
-    def __init__(self, object_type: object):
+    def __init__(self, name: str, object_type: object):
+        self.name = name
         self.object_type = object_type
         self.expressions = []
 
     def __eq__(self, __value: object) -> bool:
         if not isinstance(__value, self.object_type):
-            raise ValueError(f"Value should be of type `{self.object_type}`")
-        self.expressions.append(("==", __value))
+            raise ValueError(
+                f"`{self.name}` should be of type `{self.object_type}`"
+            )
+        return (self.name, "==", __value)
 
     def __ne__(self, __value: object) -> bool:
         if not isinstance(__value, self.object_type):
-            raise ValueError(f"Value should be of type `{self.object_type}`")
-        self.expressions.append(("!=", __value))
+            raise ValueError(
+                f"`{self.name}` should be of type `{self.object_type}`"
+            )
+        return (self.name, "!=", __value)
 
     def __lt__(self, __value: object) -> bool:
         if isinstance(__value, str):
             raise TypeError("`__lt__` does not support type `str`")
         if not isinstance(__value, self.object_type):
-            raise ValueError(f"Value should be of type `{self.object_type}`")
-        self.expressions.append(("<", __value))
+            raise ValueError(
+                f"`{self.name}` should be of type `{self.object_type}`"
+            )
+        return (self.name, "<", __value)
 
     def __gt__(self, __value: object) -> bool:
         if isinstance(__value, str):
             raise TypeError("`__gt__` does not support type `str`")
         if not isinstance(__value, self.object_type):
-            raise ValueError(f"Value should be of type `{self.object_type}`")
-        self.expressions.append((">", __value))
+            raise ValueError(
+                f"`{self.name}` should be of type `{self.object_type}`"
+            )
+        return (self.name, ">", __value)
 
     def __le__(self, __value: object) -> bool:
         if isinstance(__value, str):
             raise TypeError("`__le__` does not support type `str`")
         if not isinstance(__value, self.object_type):
-            raise ValueError(f"Value should be of type `{self.object_type}`")
-        self.expressions.append(("<=", __value))
+            raise ValueError(
+                f"`{self.name}` should be of type `{self.object_type}`"
+            )
+        return (self.name, "<=", __value)
 
     def __ge__(self, __value: object) -> bool:
         if isinstance(__value, str):
             raise TypeError("`__ge__` does not support type `str`")
         if not isinstance(__value, self.object_type):
-            raise ValueError(f"Value should be of type `{self.object_type}`")
-        self.expressions.append((">=", __value))
+            raise ValueError(
+                f"`{self.name}` should be of type `{self.object_type}`"
+            )
+        return (self.name, ">=", __value)
+
+    def in_(self, __values: list[object]) -> bool:
+        pass
+
+    def is_(self, __value: object) -> bool:
+        pass
+
+    def is_not(self, __value: object) -> bool:
+        pass
 
 
 class Metadata:
-    key = DeclarativeMapper(str)
-    value = DeclarativeMapper(str)
+    def __init__(self, name: str):
+        self.name = name
+        self.key = DeclarativeMapper(name + ".key", str)
+
+    def __getitem__(self, key: str):
+        return DeclarativeMapper(self.name + "." + key + ".value", object)
 
 
 class Dataset:
-    id = DeclarativeMapper(int)
-    name = DeclarativeMapper(str)
-    metadata = Metadata()
+    id = DeclarativeMapper("dataset.id", int)
+    name = DeclarativeMapper("dataset.name", str)
+    metadata = Metadata("dataset.metadata")
 
 
 class Model:
@@ -65,7 +91,12 @@ class Annotation:
     pass
 
 
-d = Dataset()
-d.id == 1
+expressions = [
+    Dataset.id == 1,
+    Dataset.name == "hello",
+    Dataset.metadata.key == "angle",
+    Dataset.metadata["angle"] > 0.5,
+]
 
-print(d.id.expressions)
+
+print(expressions)
