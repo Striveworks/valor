e444dc93bda7b426221986233c8521dfecf2c13b
Eric O. Korman <eric.korman@gmail.com>
2023-12-18 19:17:59 -0600

    add __version__ to velour_api using setuptools_scm

--- api/velour_api/__init__.py
+++ api/velour_api/__init__.py
@@ -1,6 +1,12 @@
+import importlib.metadata
 import logging.config
 
 from . import settings
 
 logging.config.dictConfig(settings.LogConfig().model_dump())
 logger = logging.getLogger("velour-backend")
+
+try:
+    __version__ = importlib.metadata.version("velour-api")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = ""
