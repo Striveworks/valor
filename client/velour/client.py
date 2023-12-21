3e9a1cf262576603a15a827312965b3464d80382
Charles Zaloom <czaloom4@gmail.com>
2023-12-21 15:58:57 -0600

    added docstrings to new client member functions

--- client/velour/client.py
+++ client/velour/client.py
@@ -220,12 +220,33 @@ class Client:
         self,
         dataset: dict,
     ):
+        """
+        Creates a dataset.
+
+        Parameters
+        ----------
+        dataset : dict
+            A dictionary describing dataset attributes. See `velour.coretypes.Dataset` for reference.
+        """
         self._requests_post_rel_host("datasets", json=dataset)
 
     def get_dataset(
         self,
         name: str,
     ) -> dict:
+        """
+        Gets a dataset by name.
+
+        Parameters
+        ----------
+        name : str
+            The name of the dataset to fetch.
+
+        Returns
+        -------
+        dict
+            A dictionary containing all of the associated dataset attributes.
+        """
         return self._requests_get_rel_host(f"datasets/{name}").json()
 
     def get_datasets(
@@ -245,6 +266,19 @@ class Client:
         self,
         dataset_name: str,
     ) -> List[dict]:
+        """
+        Get all datums associated with a dataset.
+
+        Parameters
+        ----------
+        dataset_name : str
+            The name of the dataset to search over.
+
+        Returns
+        -------
+        List[dict]
+            A list of dictionaries describing all the datums of the specified dataset.
+        """
         return self._requests_get_rel_host(
             f"data/dataset/{dataset_name}"
         ).json()
@@ -298,12 +332,33 @@ class Client:
         self,
         model: dict,
     ):
+        """
+        Creates a model.
+
+        Parameters
+        ----------
+        model : dict
+            A dictionary describing model attributes. See `velour.coretypes.Model` for reference.
+        """
         self._requests_post_rel_host("models", json=model)
 
     def get_model(
         self,
         name: str,
     ) -> dict:
+        """
+        Gets a model by name.
+
+        Parameters
+        ----------
+        name : str
+            The name of the model to fetch.
+
+        Returns
+        -------
+        dict
+            A dictionary containing all of the associated model attributes.
+        """
         return self._requests_get_rel_host(f"models/{name}").json()
     
     def get_models(
