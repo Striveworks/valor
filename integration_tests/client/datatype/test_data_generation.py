02c1d45307f938c86dd2833cd1adf8d52bfb63b2
Nick <nthorlind@gmail.com>
2023-12-22 09:54:13 -0700

    testing fix

--- integration_tests/client/datatype/test_data_generation.py
+++ integration_tests/client/datatype/test_data_generation.py
@@ -118,7 +118,9 @@ def test_generate_prediction_data(client: Client):
                 "iou_thresholds_to_keep": [0.0, 1.0],
             },
             "filters": {
-                **asdict(Filter()), # default filter properties with overrides below
+                **asdict(
+                    Filter()
+                ),  # default filter properties with overrides below
                 "annotation_types": ["box"],
                 "label_keys": ["k1"],
             },
