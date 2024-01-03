d8a01f306bba58747a09b457c4751326e43efad1
Charles Zaloom <czaloom4@gmail.com>
2024-01-03 11:08:21 -0600

    added tests for backend/core/labels but classifciation still failing

--- api/tests/functional-tests/backend/test_label.py
+++ api/tests/functional-tests/backend/test_label.py
@@ -7,6 +7,12 @@ from velour_api.backend import models
 from velour_api.backend.core.label import (
     fetch_matching_labels,
     create_labels,
+    get_labels,
+    get_label_keys,
+    get_joint_labels,
+    get_joint_keys,
+    get_disjoint_labels,
+    get_disjoint_keys,
 )
 
 
@@ -63,14 +69,15 @@ def create_dataset_model(db: Session, dataset_name: str, model_name: str):
     create_prediction(
         db=db,
         prediction=schemas.Prediction(
+            model=model_name,
             datum=schemas.Datum(uid="123", dataset=dataset_name),
             annotations=[
                 schemas.Annotation(
                     task_type=enums.TaskType.CLASSIFICATION,
                     labels=[
-                        schemas.Label(key="k1", value="v2"),
-                        schemas.Label(key="k1", value="v3"),
-                        schemas.Label(key="k3", value="v3")
+                        schemas.Label(key="k1", value="v2", score=0.1),
+                        schemas.Label(key="k1", value="v3", score=0.9),
+                        schemas.Label(key="k3", value="v3", score=1.0)
                     ]
                 )
             ]
@@ -143,11 +150,196 @@ def test_create_labels_with_duplicates(db: Session):
         schemas.Label(key="stoplight_color", value="red"),
     ]
     create_labels(db, labels)
-    assert len(db.query(models.schemas.Label).all()) == 1
+    assert len(db.query(models.Label).all()) == 1
 
 
 def test_get_labels(
     db: Session,
     create_dataset_model,
 ):
-    assert len()
+    assert len(db.query(models.Label).all()) == 5
+    
+    labels = get_labels(db)
+    assert len(labels) == 5
+    assert set(labels) == {
+        schemas.Label(key="k1", value="v1"),
+        schemas.Label(key="k1", value="v2"),
+        schemas.Label(key="k2", value="v3"),
+        schemas.Label(key="k1", value="v2"),
+        schemas.Label(key="k1", value="v3"),
+        schemas.Label(key="k3", value="v3")
+    }
+    
+    pred_labels = get_labels(db, ignore_groundtruths=True)
+    assert len(pred_labels) == 3
+    assert set(pred_labels) == {
+        schemas.Label(key="k1", value="v2"),
+        schemas.Label(key="k1", value="v3"),
+        schemas.Label(key="k3", value="v3")
+    }
+
+    gt_labels = get_labels(db, ignore_predictions=True)
+    assert len(gt_labels) == 3
+    assert set(gt_labels) == {
+        schemas.Label(key="k1", value="v1"),
+        schemas.Label(key="k1", value="v2"),
+        schemas.Label(key="k2", value="v3"),
+    }
+
+
+def test_get_labels_filtered(
+    db: Session,
+    create_dataset_model,
+):
+    assert len(db.query(models.Label).all()) == 5
+
+    filters = schemas.Filter(
+        label_keys=["k1"]
+    )
+    
+    labels = get_labels(db, filters=filters)
+    assert len(labels) == 3
+    assert set(labels) == {
+        schemas.Label(key="k1", value="v1"),
+        schemas.Label(key="k1", value="v2"),
+        schemas.Label(key="k1", value="v3"),
+    }
+    
+    pred_labels = get_labels(db, filters=filters, ignore_groundtruths=True)
+    assert len(pred_labels) == 2
+    assert set(pred_labels) == {
+        schemas.Label(key="k1", value="v2"),
+        schemas.Label(key="k1", value="v3"),
+    }
+
+    gt_labels = get_labels(db, filters=filters, ignore_predictions=True)
+    assert len(gt_labels) == 2
+    assert set(gt_labels) == {
+        schemas.Label(key="k1", value="v1"),
+        schemas.Label(key="k1", value="v2"),
+    }
+
+
+def test_get_label_keys(
+    db: Session,
+    create_dataset_model,
+):
+    assert len(db.query(models.Label).all()) == 5
+    
+    labels = get_label_keys(db)
+    assert len(labels) == 3
+    assert set(labels) == {"k1","k2","k3"}
+    
+    pred_labels = get_label_keys(db, ignore_groundtruths=True)
+    assert len(pred_labels) == 2
+    assert set(pred_labels) == {"k1","k3"}
+
+    gt_labels = get_label_keys(db, ignore_predictions=True)
+    assert len(gt_labels) == 2
+    assert set(gt_labels) == {"k1","k2"}
+
+
+def test_get_label_keys_filtered(
+    db: Session,
+    create_dataset_model,
+):
+    assert len(db.query(models.Label).all()) == 5
+
+    filters = schemas.Filter(
+        label_keys=["k1"]
+    )
+    
+    labels = get_label_keys(db, filters=filters)
+    assert len(labels) == 1
+    assert set(labels) == {"k1"}
+    
+    pred_labels = get_label_keys(db, filters=filters, ignore_groundtruths=True)
+    assert len(pred_labels) == 1
+    assert set(pred_labels) == {"k1"}
+
+    gt_labels = get_label_keys(db, filters=filters, ignore_predictions=True)
+    assert len(gt_labels) == 1
+    assert set(gt_labels) == {"k1"}
+
+
+def test_get_joint_labels(
+    db: Session,
+    dataset_name: str,
+    model_name: str,
+    create_dataset_model,
+):
+    labels = get_joint_labels(
+        db=db,
+        dataset_name=dataset_name,
+        model_name=model_name,
+        task_types=[enums.TaskType.CLASSIFICATION],
+        groundtruth_type=enums.AnnotationType.NONE,
+        prediction_type=enums.AnnotationType.NONE,
+    )
+    assert len(labels) == 1
+    assert labels[0] == schemas.Label(
+        key="k1",
+        value="v2",
+    )
+
+
+def test_get_joint_keys(
+    db: Session,
+    dataset_name: str,
+    model_name: str,
+    create_dataset_model,
+):
+    keys = get_joint_keys(
+        db=db,
+        dataset_name=dataset_name,
+        model_name=model_name,
+        task_type=enums.TaskType.CLASSIFICATION,
+    )
+    assert len(keys) == 1
+    assert set(keys) ==  {"k1"}
+
+
+def test_get_disjoint_labels(
+    db: Session,
+    dataset_name: str,
+    model_name: str,
+    create_dataset_model,
+):
+    ds_unique, md_unique = get_disjoint_labels(
+        db=db,
+        dataset_name=dataset_name,
+        model_name=model_name,
+        task_types=[enums.TaskType.CLASSIFICATION],
+        groundtruth_type=enums.AnnotationType.NONE,
+        prediction_type=enums.AnnotationType.NONE,
+    )
+    assert len(ds_unique) == 2
+    assert set(ds_unique) == {
+        schemas.Label(key="k1", value="v1"),
+        schemas.Label(key="k2", value="v3"),
+    }
+
+    assert len(md_unique) == 2
+    assert set(md_unique) == {
+        schemas.Label(key="k1", value="v3"),
+        schemas.Label(key="k3", value="v3") 
+    }
+
+
+def test_get_disjoint_keys(
+    db: Session,
+    dataset_name: str,
+    model_name: str,
+    create_dataset_model,
+):
+    ds_unique, md_unique = get_disjoint_keys(
+        db=db,
+        dataset_name=dataset_name,
+        model_name=model_name,
+        task_type=enums.TaskType.CLASSIFICATION,
+    )
+    assert len(ds_unique) == 1
+    assert set(ds_unique) == {"k2"}
+
+    assert len(md_unique) == 1
+    assert set(md_unique) == {"k3"}
