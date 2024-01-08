232a91d4a306362d84b1bb285b0592e0e31345c6
apalacios <jim@DESKTOP-O50T5LM.america.striveworks.us>
2024-01-08 12:32:06 -0600

    add blank line

--- api/velour_api/backend/models.py
+++ api/velour_api/backend/models.py
@@ -180,6 +180,7 @@ class Dataset(Base):
     created_at: Mapped[datetime.datetime] = mapped_column(default=func.now())
     updated_at: Mapped[datetime.datetime] = mapped_column(default=func.now(), onupdate=func.now())
 
+
 class Evaluation(Base):
     __tablename__ = "evaluation"
 
