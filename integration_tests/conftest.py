0c6e023439327fd40ff93a37bc2042d7aa8da7fc
jordanyono <jordanyono@gmail.com>
2024-01-25 17:20:26 -0500

    add defaults

--- integration_tests/conftest.py
+++ integration_tests/conftest.py
@@ -42,11 +42,11 @@ def db() -> Session:
             "Tests should be run on an empty velour backend but found existing labels."
         )
 
-    POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
     POSTGRES_USERNAME = os.getenv("POSTGRES_USERNAME", "postgres")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
     POSTGRES_DB = os.getenv("POSTGRES_DB", "velour")
-    POSTGRES_PORT = os.getenv("POSTGRES_PORT", 5432)
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
     SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
