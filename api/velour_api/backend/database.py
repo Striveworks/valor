09f6fc98cf2a9e35ea73cda1fadf8818e4f420e5
Eric O. Korman <eric.korman@gmail.com>
2024-02-09 09:47:13 -0600

    db enhancements (#420)

--- api/velour_api/backend/database.py
+++ api/velour_api/backend/database.py
@@ -1,6 +1,6 @@
 import os
 import time
-from functools import wraps
+from typing import Callable
 
 import psycopg2
 from sqlalchemy import create_engine
@@ -26,48 +26,80 @@ logger.debug(
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
-def retry_connection(timeout: int):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return func(*args, **kwargs)
-                except (
-                    psycopg2.OperationalError,
-                    OperationalError,
-                    ProgrammingError,
-                ) as e:
-                    if time.time() - start_time >= timeout:
-                        raise RuntimeError(
-                            f"Method {func.__name__} failed to connect to database within {timeout} seconds, with error: {str(e)}"
-                        )
-                time.sleep(2)
-
-        return wrapper
-
-    return decorator
-
-
-@retry_connection(30)
-def make_session() -> Session:
-    """Creates a session and enables the gdal drivers (needed for raster support)"""
-    db = sessionmaker(autocommit=False, autoflush=False, bind=engine)()
+def try_to_enable_gdal_drivers(db: Session) -> None:
+    """Tries to enable the GDAL drivers for the database. However in some cases
+    the application may not have permission to and so that must be taken care of
+    out side of this application
+    """
     try:
+        # enable for future sessions
+        db.execute(
+            text(
+                f"ALTER DATABASE {POSTGRES_DB} SET postgis.gdal_enabled_drivers = 'ENABLE_ALL';"
+            )
+        )
+
+        # enable for this session
         db.execute(text("SET postgis.gdal_enabled_drivers = 'ENABLE_ALL';"))
         db.commit()
-    except Exception as e:
+    except (psycopg2.OperationalError, OperationalError, ProgrammingError):
         db.rollback()
         db.close()
-        raise e
-    return db
 
+
+def check_db_connection(db: Session, timeout: int = 30) -> None:
+    """Check if the database connection is valid
+
+    Parameters
+    ----------
+    db : Session
+        The database connection
+    timeout : int, optional
+        The number of seconds to wait for the database to connect, by default 30
+
+    Raises
+    ------
+    RuntimeError
+        If unable to connect to the database within 30 seconds
+    """
+    start_time = time.time()
+    while True:
+        try:
+            db.execute(text("SELECT 1"))
+            break
+        except (
+            psycopg2.OperationalError,
+            OperationalError,
+            ProgrammingError,
+        ) as e:
+            if time.time() - start_time >= timeout:
+                raise RuntimeError(
+                    f"Failed to connect to database within {timeout} seconds, with error: {str(e)}"
+                )
+
+
+first_time_make_session_called = True
+
+
+def make_make_session() -> Callable[[], Session]:
+
+    first_time_make_session_called = True
+
+    def make_session() -> Session:
+        """Creates a session and enables the gdal drivers (needed for raster support). The first
+        time this is called we verify that the we can actually connect to the database.
+        """
+        nonlocal first_time_make_session_called
+        db = sessionmaker(autocommit=False, autoflush=False, bind=engine)()
+        if first_time_make_session_called:
+            check_db_connection(db)
+            try_to_enable_gdal_drivers(db)
+            first_time_make_session_called = False
+        return db
+
+    return make_session
+
+
+make_session = make_make_session()
 
 Base = declarative_base()
-
-
-@retry_connection(30)
-def create_db():
-    db = make_session()
-    db.close()
