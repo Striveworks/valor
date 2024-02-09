201677c6991bc36b99fc8159775fbc73df5ef521
Sean Bowman <r.bowman@striveworks.us>
2024-02-09 13:46:42 -0600

    Fix background logging

--- api/velour_api/logging.py
+++ api/velour_api/logging.py
@@ -1,7 +1,7 @@
 from typing import Callable
 
 import structlog
-from fastapi import Request, Response
+from fastapi import BackgroundTasks, Request, Response
 from fastapi.routing import APIRoute
 from starlette.background import BackgroundTask
 
@@ -17,13 +17,30 @@ def log_request(request: Request):
     )
 
 
+def add_task(response: Response, task: BackgroundTask):
+    if not response.background:
+        response.background = BackgroundTasks([task])
+    elif isinstance(response.background, BackgroundTasks):
+        response.background.add_task(task)
+    else:  # Empirically this doesn't happen but let's handle it anyway
+        if not isinstance(response.background, BackgroundTask):
+            logger.error(
+                "Unexpected response.background",
+                background_type=str(type(response.background)),
+            )
+        old_task = response.background
+        response.background = BackgroundTasks([old_task, task])
+
+    return response
+
+
 class LoggingRoute(APIRoute):
     def get_route_handler(self) -> Callable:
         original_route_handler = super().get_route_handler()
 
         async def custom_route_handler(request: Request) -> Response:
             response = await original_route_handler(request)
-            BackgroundTask(log_request, request)
-            return response
+            task = BackgroundTask(log_request, request)
+            return add_task(response, task)
 
         return custom_route_handler
