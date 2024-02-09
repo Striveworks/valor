3ab4e750aa9be744aa9d21688100c290a858bb36
jordanyono <jordanyono@gmail.com>
2024-02-09 16:26:17 -0500

    add cascades and drops

--- migrations/sql/00000001_initialize_schema.down.sql
+++ migrations/sql/00000001_initialize_schema.down.sql
@@ -1,19 +1,19 @@
-drop table metric;
+drop table metric cascade;
 
-drop table confusion_matrix;
+drop table confusion_matrix cascade;
 
-drop table evaluation;
+drop table evaluation cascade;
 
-drop table groundtruth;
+drop table groundtruth cascade;
 
-drop table prediction;
+drop table prediction cascade;
 
-drop table label;
+drop table label cascade;
 
-drop table annotation;
+drop table annotation cascade;
 
-drop table model;
+drop table model cascade;
 
-drop table datum;
+drop table datum cascade;
 
-drop table dataset;
+drop table dataset cascade;
