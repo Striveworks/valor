8523d5094c8799dcd5c0e9a0dd832acfd06bd1bd
Sean Bowman <r.bowman@striveworks.us>
2024-02-12 08:27:00 -0600

    Log response status code in endpoint logging

--- api/velour_api/logging.py
+++ api/velour_api/logging.py
@@ -1,27 +1,30 @@
-from typing import Callable
+from typing import Callable, Optional
 
 import structlog
-from fastapi import BackgroundTasks, Request, Response
+from fastapi import BackgroundTasks, HTTPException, Request, Response
 from fastapi.routing import APIRoute
 from starlette.background import BackgroundTask
 
 logger = structlog.get_logger()
 
 
-def log_request(
-    request: Request, ignore_paths=frozenset(["/health", "/ready"])
+def log_endpoint(
+    request: Request,
+    status_code: Optional[int],
+    ignore_paths=frozenset(["/health", "/ready"]),
 ):
-    if request.url.path in ignore_paths:
+    if request.url.path in ignore_paths and status_code == 200:
         return
     logger.info(
         "Velour API Call",
         method=request.method,
         path=request.url.path,
         hostname=request.url.hostname,
+        status=status_code,
     )
 
 
-def add_task(response: Response, task: BackgroundTask):
+def add_task(response: Response, task: BackgroundTask) -> Response:
     if not response.background:
         response.background = BackgroundTasks([task])
     elif isinstance(response.background, BackgroundTasks):
@@ -43,8 +46,19 @@ class LoggingRoute(APIRoute):
         original_route_handler = super().get_route_handler()
 
         async def custom_route_handler(request: Request) -> Response:
-            response = await original_route_handler(request)
-            task = BackgroundTask(log_request, request)
+            try:
+                response = await original_route_handler(request)
+            except HTTPException as e:
+                log_endpoint(request, e.status_code)
+                raise
+            except Exception:
+                log_endpoint(request, None)
+                logger.exception("Uncaught exception")
+                raise
+            else:
+                task = BackgroundTask(
+                    log_endpoint, request, response.status_code
+                )
             return add_task(response, task)
 
         return custom_route_handler
