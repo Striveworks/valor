a87674ac1fcc99e9affa3d67938ae5b2a3f59e48
Sean Bowman <r.bowman@striveworks.us>
2024-02-20 10:27:39 -0600

    Remove unused logging config

--- api/valor_api/settings.py
+++ api/valor_api/settings.py
@@ -2,43 +2,6 @@ from pydantic import ConfigDict, model_validator
 from pydantic_settings import BaseSettings
 
 
-class LogConfig(BaseSettings):
-    """Logging configuration to be set for the server
-
-    (taken from https://stackoverflow.com/a/67937084 but using
-    BaseSettings)
-    """
-
-    LOGGER_NAME: str = "valor-backend"
-    LOG_FORMAT: str = "%(levelprefix)s | %(asctime)s | %(message)s"
-    LOG_LEVEL: str = "DEBUG"
-
-    # Logging config
-    version: int = 1
-    disable_existing_loggers: bool = False
-    formatters: dict = {
-        "default": {
-            "()": "uvicorn.logging.DefaultFormatter",
-            "fmt": LOG_FORMAT,
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-        },
-    }
-    handlers: dict = {
-        "default": {
-            "formatter": "default",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stderr",
-        },
-    }
-    loggers: dict = {
-        LOGGER_NAME: {
-            "handlers": ["default"],
-            "level": LOG_LEVEL,
-            "propagate": False,
-        },
-    }
-
-
 class AuthConfig(BaseSettings):
     SECRET_KEY: str | None = None
     ALGORITHM: str | None = "HS256"
