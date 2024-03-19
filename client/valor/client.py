18f152db651e100825eb99ac2935f2f59517663c
Nick L <nthorlind@gmail.com>
2024-03-19 17:19:58 -0600

    Improve test coverage to 90% and enforce coverage check in `combine-coverage-report` workflow (#489)

--- client/valor/client.py
+++ client/valor/client.py
@@ -91,7 +91,7 @@ def get_json_size(json_obj: object, encoding: str = "utf-8") -> int:
     )
 
 
-def chunk_list(
+def _chunk_list(
     json_list: list, chunk_size_bytes: int, encoding: str = "utf-8"
 ) -> List[list]:
     """
@@ -122,6 +122,11 @@ def chunk_list(
     n_elements = len(json_list)
     avg_element_size = get_json_size(json_list) / n_elements
     n_elements_per_chunk = int(chunk_size_bytes / avg_element_size)
+
+    # return a list of individual elements if n_elements_per_chunk is zero
+    if n_elements_per_chunk == 0:
+        return [[element] for element in json_list]
+
     n_chunks = math.ceil(n_elements / n_elements_per_chunk)
 
     chunks = []
@@ -135,7 +140,7 @@ def chunk_list(
             new_chunk_size > chunk_size_bytes
         ):  # Recursively chunk if still too large.
             chunks.extend(
-                chunk_list(
+                _chunk_list(
                     json_list=json_list[lhi:rhi],
                     chunk_size_bytes=chunk_size_bytes,
                     encoding=encoding,
@@ -357,7 +362,7 @@ class ClientConnection:
         chunk_size_bytes : int, default=1e7
             Maximum size of a POST'ed json in bytes. Defaults to 10MB.
         """
-        chunked_groundtruths = chunk_list(
+        chunked_groundtruths = _chunk_list(
             json_list=groundtruths,
             chunk_size_bytes=chunk_size_bytes,
         )
@@ -410,7 +415,7 @@ class ClientConnection:
         chunk_size_bytes : int, default=1e7
             Maximum size of a POST'ed json in bytes. Defaults to 10MB.
         """
-        chunked_predictions = chunk_list(
+        chunked_predictions = _chunk_list(
             json_list=predictions,
             chunk_size_bytes=chunk_size_bytes,
         )
