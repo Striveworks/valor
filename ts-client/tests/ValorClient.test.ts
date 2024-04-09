a9cc61a3a571001c416b48d5fdad5c0352857466
Nick <nthorlind@gmail.com>
2024-04-09 10:59:25 -0600

    quick check

--- ts-client/tests/ValorClient.test.ts
+++ ts-client/tests/ValorClient.test.ts
@@ -143,11 +143,11 @@ test('evaluation methods', async () => {
       modelName,
       datasetName,
       'classification',
-      [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95],
-      [0.5, 0.75],
       null,
       null,
-      true,
+      null,
+      null,
+      null,
       null
     );
     expect(['running', 'pending', 'done']).toContain(evaluation.status);
