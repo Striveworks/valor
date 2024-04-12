0452efae3392b102c91f1daaeabe600038987ffe
Charles Zaloom <czaloom4@gmail.com>
2024-04-12 13:46:42 -0500

    added migration steps

---
+++ migrations/sql/00000003_remove_multipolygon_filter.down.sql
@@ -0,0 +1,2 @@
+UPDATE evaluation SET datum_filter = jsonb_set(datum_filter, '{multipolygon_area}', 'null', true);
+UPDATE evaluation SET datum_filter = jsonb_set(datum_filter, '{require_multipolygon}', 'null', true);
---
+++ migrations/sql/00000003_remove_multipolygon_filter.up.sql
@@ -0,0 +1,2 @@
+UPDATE evaluation SET datum_filter = datum_filter - 'multipolygon_area';
+UPDATE evaluation SET datum_filter = datum_filter - 'require_multipolygon';
