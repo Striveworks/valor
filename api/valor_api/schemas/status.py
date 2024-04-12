6f95afe82638c74b8ecde7f40213668bc2513493
Charles Zaloom <czaloom4@gmail.com>
2024-04-12 16:32:36 -0500

    revert reorg of APIVersion schema

--- api/valor_api/schemas/status.py
+++ api/valor_api/schemas/status.py
@@ -1,19 +1,6 @@
 from pydantic import BaseModel
 
 
-class APIVersion(BaseModel):
-    """
-    Defines an API version string which is sent back to the user after their authentication is confirmed.
-
-    Attributes
-    ----------
-    api_version : str
-        The API version.
-    """
-
-    api_version: str
-
-
 class Health(BaseModel):
     """
     Info regarding the health of the service.
