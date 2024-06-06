5e713ba811e6e8cc1da6e566ee30e7dea65d94c9
Charles Zaloom <czaloom4@gmail.com>
2024-06-06 13:14:59 -0500

    added data migration

--- migrations/sql/00000008_replace_annotation_task_type.up.sql
+++ migrations/sql/00000008_replace_annotation_task_type.up.sql
@@ -1,3 +1,7 @@
-ALTER TABLE annotation DROP COLUMN task_type;
 ALTER TABLE annotation ADD COLUMN is_instance boolean;
 ALTER TABLE annotation ADD COLUMN implied_task_types jsonb;
+
+UPDATE annotation
+SET implied_task_types = jsonb_build_array(task_type);
+
+ALTER TABLE annotation DROP COLUMN task_type;
