ca266bd3730ad9c79e681e11cf7b47900f380d59
Charles Zaloom <czaloom4@gmail.com>
2024-06-10 11:26:53 -0500

    adjusted migration

--- migrations/sql/00000009_add_dataset_names.up.sql
+++ migrations/sql/00000009_add_dataset_names.up.sql
@@ -2,3 +2,6 @@ ALTER TABLE evaluation ADD COLUMN dataset_names jsonb;
 
 UPDATE evaluation
 SET dataset_names = datum_filter->'dataset_names';
+
+UPDATE evaluation
+SET datum_filter->'dataset_names' = NULL;
