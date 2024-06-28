1d2db2b7421c635c39c3198934c86dbae061f949
Nick <nthorlind@gmail.com>
2024-06-28 15:43:13 -0600

    get detection test working

--- integration_tests/benchmarks/object-detection/benchmark_script.py
+++ integration_tests/benchmarks/object-detection/benchmark_script.py
@@ -1,322 +1,267 @@
-# # %%
+# %%
+import json
+import os
+from datetime import datetime
+from time import time
 
-# with annotations AS (
-# 	select id as annotation_id,
-# 		datum_id,
-# 		ST_AsGeoJSON(ST_Envelope(ST_MinConvexHull(raster))) as "raster",
-# 		ST_AsGeoJSON(ST_Envelope(polygon)) as "polygon",
-# 		ST_AsGeoJSON(box) as "box",
-# 		is_instance
-# 	from annotation a
-# ),
-# annotations_with_labels as (
-# 	select datum_id,
-# 		b.id as groundtruth_id,
-# 		a.annotation_id,
-# 		raster,
-# 		polygon,
-# 		box,
-# 		is_instance,
-# 		array_agg(jsonb_build_object('key', c.key, 'value', c.value)) as labels
-# 	from annotations a
-# 	inner join groundtruth b on a.annotation_id=b.annotation_id
-# 	inner join label c on b.label_id=c.id
-# 	group by datum_id, a.annotation_id, b.id, raster, polygon, box, is_instance
-# )
-# select datum_id,
-# 	groundtruth_id,
-# 	array_agg(jsonb_build_object('raster', raster, 'polygon', polygon, 'box', box, 'labels', labels, 'is_instance', is_instance)) as anns
-# from annotations_with_labels
-# group by datum_id, groundtruth_id, annotation_id
-# order by datum_id desc, groundtruth_id desc
+from valor import (
+    Annotation,
+    Client,
+    Dataset,
+    Datum,
+    GroundTruth,
+    Label,
+    Model,
+    Prediction,
+    connect,
+)
+from valor.schemas import MultiPolygon, Polygon, Raster
 
-# # %%
+connect("http://0.0.0.0:8000")
+client = Client()
 
 
-# import json
-# import os
-# from datetime import datetime
-# from time import time
-
-# from valor import (
-#     Annotation,
-#     Client,
-#     Dataset,
-#     Datum,
-#     GroundTruth,
-#     Label,
-#     Model,
-#     Prediction,
-#     connect,
-# )
-
-# connect("http://0.0.0.0:8000")
-# client = Client()
-
-# dset = Dataset.create(name="coco")
-# model = Model.create(name="some_model")
-
-# # %%
+def _convert_wkt_to_coordinates(wkt: str) -> list[list[tuple]]:
+    """Convert a WKT string into a nested list of coordinates."""
+    return [
+        [tuple(float(y) for y in x) for x in json.loads(wkt)["coordinates"][0]]
+    ]
 
 
-# results_file: str = "results.json"
-# data_file: str = "data.json"
-# pair_limit = 1
-# current_directory = os.path.dirname(os.path.realpath(__file__))
-# write_path = f"{current_directory}/{results_file}"
-# read_path = f"{current_directory}/{data_file}"
+def write_results_to_file(write_path: str, result_dict: dict):
+    """Write results to results.json"""
+    current_datetime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+    if os.path.isfile(write_path):
+        with open(write_path, "r") as file:
+            file.seek(0)
+            data = json.load(file)
+    else:
+        data = {}
+
+    data[current_datetime] = result_dict
+
+    with open(write_path, "w+") as file:
+        json.dump(data, file, indent=4)
 
 
-# with open(read_path) as f:
-#     raw_data = json.load(f)
+def ingest_groundtruths_and_predictions(
+    dset: Dataset, model: Model, raw: list, pair_limit: int
+):
+    """Ingest the data into Valor."""
+    groundtruths = []
+    predictions = []
 
-# # %%
-# groundtruths = []
-# for gt in raw_data["groundtruths"][:pair_limit]:
-#     print(gt)
-#     groundtruths.append(
-#         GroundTruth(
-#             datum=Datum(
-#                 uid=gt["datum_uid"],
-#                 metadata={
-#                     "width": 640,
-#                     "height": 426,
-#                     "license": 2,
-#                 },
-#                 metadata={"width": 224, "height": 224},
-#             ),
-#             annotations=[
-#                 Annotation(
-#                     labels=[
-#                         Label(
-#                             key=label["key"],
-#                             value=label["value"],
-#                             score=label["score"],
-#                         )
-#                         for label in annotation["labels"]
-#                     ],
-#                 )
-#                 for annotation in groundtruth["value"]["annotations"]
-#             ],
-#         )
-#     )
+    for datum_id, data in raw[:pair_limit]:
+        datum = Datum(
+            uid=str(datum_id),
+            metadata=data["datum_metadata"],
+        )
+        groundtruths.append(
+            GroundTruth(
+                datum=datum,
+                annotations=list(
+                    [
+                        Annotation(
+                            is_instance=ann["is_instance"],
+                            labels=list(
+                                [
+                                    Label(
+                                        key=label["key"],
+                                        value=label["value"],
+                                    )
+                                    for label in ann["labels"]
+                                ]
+                            ),
+                            bounding_box=(
+                                _convert_wkt_to_coordinates(ann["box"])
+                                if ann["box"]
+                                else None
+                            ),
+                            raster=(
+                                Raster.from_geometry(
+                                    geometry=MultiPolygon(
+                                        [
+                                            _convert_wkt_to_coordinates(
+                                                ann["raster"]
+                                            )
+                                        ]
+                                    ),
+                                    height=data["datum_metadata"]["height"],
+                                    width=data["datum_metadata"]["width"],
+                                )
+                                if ann["raster"]
+                                else None
+                            ),
+                            polygon=(
+                                (
+                                    Polygon(
+                                        _convert_wkt_to_coordinates(
+                                            ann["polygon"]
+                                        )
+                                    )
+                                )
+                                if ann["polygon"]
+                                else None
+                            ),
+                        )
+                        for ann in data["groundtruth_annotations"]
+                    ]
+                ),
+            )
+        )
 
-# # %%
-# len(raw_data["groundtruths"])
+        predictions.append(
+            Prediction(
+                datum=datum,
+                annotations=list(
+                    [
+                        Annotation(
+                            is_instance=ann["is_instance"],
+                            labels=list(
+                                [
+                                    Label(
+                                        key=label["key"],
+                                        value=label["value"],
+                                        score=label["score"],
+                                    )
+                                    for label in ann["labels"]
+                                ]
+                            ),
+                            bounding_box=(
+                                _convert_wkt_to_coordinates(ann["box"])
+                                if ann["box"]
+                                else None
+                            ),
+                            raster=(
+                                Raster.from_geometry(
+                                    geometry=MultiPolygon(
+                                        [
+                                            _convert_wkt_to_coordinates(
+                                                ann["raster"]
+                                            )
+                                        ]
+                                    ),
+                                    height=data["datum_metadata"]["height"],
+                                    width=data["datum_metadata"]["width"],
+                                )
+                                if ann["raster"]
+                                else None
+                            ),
+                            polygon=(
+                                (
+                                    Polygon(
+                                        _convert_wkt_to_coordinates(
+                                            ann["polygon"]
+                                        )
+                                    )
+                                )
+                                if ann["polygon"]
+                                else None
+                            ),
+                        )
+                        for ann in data["prediction_annotations"]
+                    ]
+                ),
+            )
+        )
 
+    for gt in groundtruths:
+        dset.add_groundtruth(gt)
 
-# # %%
-# raw_data
-# # %%
-# for y in ["groundtruths", "predictions"]:
-#     for x in raw_data[y]:
-#         try:
-#             del x["id"]
-#         except:
-#             pass
+    for pred in predictions:
+        model.add_prediction(dset, pred)
 
-# # %%
-# from itertools import groupby
-
-# raw_data.sort(key=lambda row: (row["datum_uid"],))
-
-# # then use groupby with the same key
-# groups = groupby(contents, lambda content: content["adult"])
-
-# # %%
-# set(
-#     [
-#         tuple(x["implied_task_types"])
-#         for x in raw_data["groundtruths"]
-#         if x["is_instance"] == False
-#     ]
-# )
-
-# # start_time = time()
-
-# # ingest_groundtruths_and_predictions(
-# #     dset=dset, model=model, raw=raw_data, pair_limit=limit
-# # )
-# # ingest_time = time() - start_time
-
-# # run_base_evaluation(dset=dset, model=model)
-# # ingest_and_evaluation = time() - start_time
-
-# # results = {
-# #     "number_of_datums": limit,
-# #     "ingest_runtime": f"{(ingest_time):.1f} seconds",
-# #     "ingest_and_evaluation_runtime": f"{(ingest_and_evaluation):.1f} seconds",
-# # }
-# # write_results_to_file(write_path=write_path, result_dict=results)
-
-# # client.delete_dataset(dset.name, timeout=30)
-# # client.delete_model(model.name, timeout=30)
-
-
-# # %%
-# def write_results_to_file(write_path: str, result_dict: dict):
-#     """Write results to results.json"""
-#     current_datetime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-
-#     if os.path.isfile(write_path):
-#         with open(write_path, "r") as file:
-#             file.seek(0)
-#             data = json.load(file)
-#     else:
-#         data = {}
-
-#     data[current_datetime] = result_dict
-
-#     with open(write_path, "w+") as file:
-#         json.dump(data, file, indent=4)
-
-
-# def ingest_groundtruths_and_predictions(
-#     dset: Dataset, model: Model, raw: dict, pair_limit: int
-# ):
-#     """Ingest the data into Valor."""
-
-#     groundtruths = []
-#     predictions = []
-#     slice_ = (
-#         raw["groundtruth_prediction_pairs"][:pair_limit]
-#         if pair_limit != -1
-#         else raw["groundtruth_prediction_pairs"]
-#     )
-#     for groundtruth, prediction in slice_:
-#         groundtruths.append(
-#             GroundTruth(
-#                 datum=Datum(
-#                     uid=groundtruth["value"]["datum"]["uid"],
-#                     metadata={"width": 224, "height": 224},
-#                 ),
-#                 annotations=[
-#                     Annotation(
-#                         labels=[
-#                             Label(
-#                                 key=label["key"],
-#                                 value=label["value"],
-#                                 score=label["score"],
-#                             )
-#                             for label in annotation["labels"]
-#                         ],
-#                     )
-#                     for annotation in groundtruth["value"]["annotations"]
-#                 ],
-#             )
-#         )
-
-#         predictions.append(
-#             Prediction(
-#                 datum=Datum(
-#                     uid=prediction["value"]["datum"]["uid"],
-#                     metadata={"width": 224, "height": 224},
-#                 ),
-#                 annotations=[
-#                     Annotation(
-#                         labels=[
-#                             Label(
-#                                 key=label["key"],
-#                                 value=label["value"],
-#                                 score=label["score"],
-#                             )
-#                             for label in annotation["labels"]
-#                         ],
-#                     )
-#                     for annotation in prediction["value"]["annotations"]
-#                 ],
-#             )
-#         )
-
-#     for gt in groundtruths:
-#         dset.add_groundtruth(gt)
-
-#     for pred in predictions:
-#         model.add_prediction(dset, pred)
-
-#     dset.finalize()
-#     model.finalize_inferences(dataset=dset)
+    dset.finalize()
+    model.finalize_inferences(dataset=dset)
 
 
-# def run_base_evaluation(dset: Dataset, model: Model):
-#     """Run a base evaluation (with no PR curves)."""
-#     evaluation = model.evaluate_classification(dset)
-#     evaluation.wait_for_completion()
-#     return evaluation
+def run_base_evaluation(dset: Dataset, model: Model):
+    """Run a base evaluation (with no PR curves)."""
+    evaluation = model.evaluate_detection(dset)
+    evaluation.wait_for_completion()
+    return evaluation
 
 
-# def run_pr_curve_evaluation(dset: Dataset, model: Model):
-#     """Run a base evaluation with PrecisionRecallCurve included."""
-#     evaluation = model.evaluate_classification(
-#         dset,
-#         metrics_to_return=[
-#             "Accuracy",
-#             "Precision",
-#             "Recall",
-#             "F1",
-#             "ROCAUC",
-#             "PrecisionRecallCurve",
-#         ],
-#     )
-#     evaluation.wait_for_completion()
-#     return evaluation
+# TODO fix metrics
+def run_pr_curve_evaluation(dset: Dataset, model: Model):
+    """Run a base evaluation with PrecisionRecallCurve included."""
+    evaluation = model.evaluate_detection(
+        dset,
+        metrics_to_return=[
+            "Accuracy",
+            "Precision",
+            "Recall",
+            "F1",
+            "ROCAUC",
+            "PrecisionRecallCurve",
+        ],
+    )
+    evaluation.wait_for_completion()
+    return evaluation
 
 
-# def run_detailed_pr_curve_evaluation(dset: Dataset, model: Model):
-#     """Run a base evaluation with PrecisionRecallCurve and DetailedPrecisionRecallCurve included."""
+# TODO fix metrics
+def run_detailed_pr_curve_evaluation(dset: Dataset, model: Model):
+    """Run a base evaluation with PrecisionRecallCurve and DetailedPrecisionRecallCurve included."""
 
-#     evaluation = model.evaluate_classification(
-#         dset,
-#         metrics_to_return=[
-#             "Accuracy",
-#             "Precision",
-#             "Recall",
-#             "F1",
-#             "ROCAUC",
-#             "PrecisionRecallCurve",
-#             "DetailedPrecisionRecallCurve",
-#         ],
-#     )
-#     evaluation.wait_for_completion()
-#     return evaluation
-
-
-# def run_benchmarking_analysis(
-#     limits_to_test: list[int] = [100, 100, 100],
-#     results_file: str = "results.json",
-#     data_file: str = "data.json",
-# ):
-#     """Time various function calls and export the results."""
-#     current_directory = os.path.dirname(os.path.realpath(__file__))
-#     write_path = f"{current_directory}/{results_file}"
-#     read_path = f"{current_directory}/{data_file}"
-#     for limit in limits_to_test:
-#         dset = Dataset.create(name="bird-identification")
-#         model = Model.create(name="some_model")
-
-#         with open(read_path) as f:
-#             raw_data = json.load(f)
-
-#         start_time = time()
-
-#         ingest_groundtruths_and_predictions(
-#             dset=dset, model=model, raw=raw_data, pair_limit=limit
-#         )
-#         ingest_time = time() - start_time
-
-#         run_base_evaluation(dset=dset, model=model)
-#         ingest_and_evaluation = time() - start_time
-
-#         results = {
-#             "number_of_datums": limit,
-#             "ingest_runtime": f"{(ingest_time):.1f} seconds",
-#             "ingest_and_evaluation_runtime": f"{(ingest_and_evaluation):.1f} seconds",
-#         }
-#         write_results_to_file(write_path=write_path, result_dict=results)
-
-#         client.delete_dataset(dset.name, timeout=30)
-#         client.delete_model(model.name, timeout=30)
+    evaluation = model.evaluate_detection(
+        dset,
+        metrics_to_return=[
+            "Accuracy",
+            "Precision",
+            "Recall",
+            "F1",
+            "ROCAUC",
+            "PrecisionRecallCurve",
+            "DetailedPrecisionRecallCurve",
+        ],
+    )
+    evaluation.wait_for_completion()
+    return evaluation
 
 
-# if __name__ == "__main__":
-#     run_benchmarking_analysis()
+def run_benchmarking_analysis(
+    limits_to_test: list[int] = [1, 100, 100, 500, 500],  # TODO
+    results_file: str = "results.json",
+    data_file: str = "data_500_datums.json",
+):
+    """Time various function calls and export the results."""
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    write_path = f"{current_directory}/{results_file}"
+    read_path = f"{current_directory}/{data_file}"
+    for limit in limits_to_test:
+        dset = Dataset.create(name="coco-dataset")
+        model = Model.create(name="coco-model")
+
+        with open(read_path) as f:
+            raw_data = json.load(f)
+
+        # convert dict into list of tuples so we can slice it
+        raw_data_tuple = [(key, value) for key, value in raw_data.items()]
+
+        start_time = time()
+
+        ingest_groundtruths_and_predictions(
+            dset=dset, model=model, raw=raw_data_tuple, pair_limit=limit
+        )
+        ingest_time = time() - start_time
+
+        run_base_evaluation(dset=dset, model=model)
+        ingest_and_evaluation = time() - start_time
+
+        results = {
+            "number_of_datums": limit,
+            "ingest_runtime": f"{(ingest_time):.1f} seconds",
+            "ingest_and_evaluation_runtime": f"{(ingest_and_evaluation):.1f} seconds",
+        }
+        write_results_to_file(write_path=write_path, result_dict=results)
+
+        client.delete_dataset(dset.name, timeout=30)
+        client.delete_model(model.name, timeout=30)
+
+
+if __name__ == "__main__":
+    run_benchmarking_analysis()
+
+# %%
