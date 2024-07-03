df41476f1eca89c24b3aa2abb86afc0129eab888
Nick <nthorlind@gmail.com>
2024-07-03 12:56:28 -0600

    try new limits

--- integration_tests/benchmarks/object-detection/benchmark_script.py
+++ integration_tests/benchmarks/object-detection/benchmark_script.py
@@ -233,7 +233,7 @@ def run_detailed_pr_curve_evaluation(dset: Dataset, model: Model):
 
 
 def run_benchmarking_analysis(
-    limits_to_test: list[int] = [5, 5],
+    limits_to_test: list[int] = [1, 2, 3, 4],
     results_file: str = "results.json",
     data_file: str = "data.json",
 ):
