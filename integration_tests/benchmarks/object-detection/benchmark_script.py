6dc83bcae24e9e343167ea1fa895acd7b703ffbf
Nick <nthorlind@gmail.com>
2024-07-17 10:32:00 -0600

    update od benchmarks

--- integration_tests/benchmarks/object-detection/benchmark_script.py
+++ integration_tests/benchmarks/object-detection/benchmark_script.py
@@ -233,7 +233,7 @@ def run_detailed_pr_curve_evaluation(dset: Dataset, model: Model):
 
 
 def run_benchmarking_analysis(
-    limits_to_test: list[int] = [3, 7, 10, 15, 20, 100],
+    limits_to_test: list[int] = [3, 7, 10, 15, 20, 45, 60],
     results_file: str = "results.json",
     data_file: str = "data.json",
 ):
