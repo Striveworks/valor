a286cdf281e626d965640defc7cf3a8133418f61
Charles Zaloom <czaloom4@gmail.com>
2024-08-01 16:53:46 -0500

    revert migrations

--- migrations/sql/00000012_add_datum_text.down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE datum DROP COLUMN textblob;
-ALTER TABLE annotation DROP COLUMN context;
--- migrations/sql/00000012_add_datum_text.up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE datum ADD COLUMN textblob text;
-ALTER TABLE annotation ADD COLUMN context jsonb;
