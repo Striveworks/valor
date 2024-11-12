566fe297a3ad43bf8e13e1aadfbb64332654696d
Charles Zaloom <czaloom4@gmail.com>
2024-11-12 11:48:35 -0600

    added new benchmark workflow

--- lite/benchmarks/synthetic/benchmark_semantic_segmentation.py
+++ lite/benchmarks/synthetic/benchmark_semantic_segmentation.py
@@ -3,11 +3,11 @@ from valor_lite.semantic_segmentation.benchmark import benchmark
 if __name__ == "__main__":
 
     benchmark(
-        bitmask_shape=(10000, 10000),
+        bitmask_shape=(4000, 4000),
         number_of_images=10,
         number_of_unique_labels=10,
         memory_limit=4.0,
         time_limit=10.0,
         repeat=1,
-        verbose=False,
+        verbose=True,
     )
---
+++ lite/benchmarks/synthetic/benchmark_that_fails.py
@@ -0,0 +1,13 @@
+from valor_lite.semantic_segmentation.benchmark import benchmark
+
+if __name__ == "__main__":
+
+    benchmark(
+        bitmask_shape=(10000, 10000),
+        number_of_images=10,
+        number_of_unique_labels=10,
+        memory_limit=4.0,
+        time_limit=10.0,
+        repeat=1,
+        verbose=True,
+    )
