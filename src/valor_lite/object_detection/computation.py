fix precision and remove accuracy for obj det (#836)

src/valor_lite/object_detection/computation.py
@@ -282,7 +282,6 @@ def compute_precion_recall(
     ],
     NDArray[np.float64],
     NDArray[np.float64],
-    NDArray[np.float64],
 ]:
     """
     Computes Object Detection metrics.
@@ -315,8 +314,6 @@ def compute_precion_recall(
     tuple[NDArray[np.float64], NDArray[np.float64], NDArray[np.float64], float]
         Average Recall results.
     NDArray[np.float64]
-        Accuracy.
-    NDArray[np.float64]
         Precision, Recall, TP, FP, FN, F1 Score.
     NDArray[np.float64]
         Interpolated Precision-Recall Curves.
@@ -334,7 +331,6 @@ def compute_precion_recall(
 
     average_precision = np.zeros((n_ious, n_labels), dtype=np.float64)
     average_recall = np.zeros((n_scores, n_labels), dtype=np.float64)
-    accuracy = np.zeros((n_ious, n_scores), dtype=np.float64)
     counts = np.zeros((n_ious, n_scores, n_labels, 6), dtype=np.float64)
 
     pd_labels = data[:, 5].astype(np.int32)
@@ -383,37 +379,38 @@ def compute_precion_recall(
             )
             mask_gt_unique = np.zeros(tp_candidates.shape[0], dtype=np.bool_)
             mask_gt_unique[indices_gt_unique] = True
+
             true_positives_mask = np.zeros(n_rows, dtype=np.bool_)
             true_positives_mask[mask_tp_inner] = mask_gt_unique
 
+            mask_fp_inner |= mask_tp_inner & ~true_positives_mask
+
             # calculate intermediates
-            pd_count = np.bincount(pd_labels, minlength=n_labels).astype(
-                np.float64
-            )
             tp_count = np.bincount(
                 pd_labels,
                 weights=true_positives_mask,
                 minlength=n_labels,
             ).astype(np.float64)
-
             fp_count = np.bincount(
                 pd_labels[mask_fp_inner],
                 minlength=n_labels,
             ).astype(np.float64)
-
             fn_count = np.bincount(
                 pd_labels[mask_fn_inner],
                 minlength=n_labels,
             )
+
+            fn_count = gt_count - tp_count
+            tp_fp_count = tp_count + fp_count
 
             # calculate component metrics
             recall = np.zeros_like(tp_count)
             np.divide(tp_count, gt_count, where=gt_count > 1e-9, out=recall)
 
             precision = np.zeros_like(tp_count)
-            np.divide(tp_count, pd_count, where=pd_count > 1e-9, out=precision)
-
-            fn_count = gt_count - tp_count
+            np.divide(
+                tp_count, tp_fp_count, where=tp_fp_count > 1e-9, out=precision
+            )
 
             f1_score = np.zeros_like(precision)
             np.divide(
@@ -434,14 +431,6 @@ def compute_precion_recall(
                     f1_score[:, np.newaxis],
                 ),
                 axis=1,
-            )
-
-            # caluculate accuracy
-            total_pd_count = label_metadata[:, 1].sum()
-            accuracy[iou_idx, score_idx] = (
-                (tp_count.sum() / total_pd_count)
-                if total_pd_count > 1e-9
-                else 0.0
             )
 
             # calculate recall for AR
@@ -562,7 +551,6 @@ def compute_precion_recall(
     return (
         ap_results,  # type: ignore[reportReturnType]
         ar_results,
-        accuracy,
         counts,
         pr_curve,
     )

tests/object_detection/test_average_recall.py
@@ -26,7 +26,7 @@ def test__compute_average_recall():
     iou_thresholds = np.array([0.1, 0.6])
     score_thresholds = np.array([0.5, 0.93, 0.98])
 
-    (_, results, _, _, _,) = compute_precion_recall(
+    (_, results, _, _,) = compute_precion_recall(
         sorted_pairs,
         label_metadata=label_metadata,
         iou_thresholds=iou_thresholds,
@@ -497,7 +497,7 @@ def test_ar_true_positive_deassignment(
         score_thresholds=[0.5],
     )
 
-    assert len(metrics) == 15
+    assert len(metrics) == 14
 
     # test AR
     actual_metrics = [m.to_dict() for m in metrics[MetricType.AR]]

tests/object_detection/test_precision.py
@@ -21,7 +21,7 @@ def test__compute_precision():
     iou_thresholds = np.array([0.1, 0.6])
     score_thresholds = np.array([0.0])
 
-    (_, _, _, counts, _) = compute_precion_recall(
+    (_, _, counts, _) = compute_precion_recall(
         sorted_pairs,
         label_metadata=label_metadata,
         iou_thresholds=iou_thresholds,
@@ -469,3 +469,25 @@ def test_precision_false_negatives_two_datums_one_only_with_different_class_high
         assert m in expected_metrics
     for m in expected_metrics:
         assert m in actual_metrics
+
+
+def test_precision_model_one_class_spam_fp(
+    detections_model_single_class_spam_fp: list[Detection],
+):
+    loader = DataLoader()
+    loader.add_bounding_boxes(detections_model_single_class_spam_fp)
+    evaluator = loader.finalize()
+    metrics = evaluator.evaluate(score_thresholds=[0.5], iou_thresholds=[0.5])
+    actual_metrics = [m.to_dict() for m in metrics[MetricType.Precision]]
+    expected_metrics = [
+        {
+            "type": "Precision",
+            "value": 1.0,
+            "parameters": {
+                "iou_threshold": 0.5,
+                "score_threshold": 0.5,
+                "label": "dog",
+            },
+        },
+    ]
+    assert actual_metrics == expected_metrics