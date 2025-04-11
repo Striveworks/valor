90c1489f85a70b89a1374a71c88ead763a92f4ac
Charles Zaloom <czaloom4@gmail.com>
2025-04-11 18:05:12 -0400

    remove accuracy

--- src/valor_lite/object_detection/computation.py
+++ src/valor_lite/object_detection/computation.py
@@ -282,7 +282,6 @@ def compute_precion_recall(
     ],
     NDArray[np.float64],
     NDArray[np.float64],
-    NDArray[np.float64],
 ]:
     """
     Computes Object Detection metrics.
@@ -315,8 +314,6 @@ def compute_precion_recall(
     tuple[NDArray[np.float64], NDArray[np.float64], NDArray[np.float64], float]
         Average Recall results.
     NDArray[np.float64]
-        Accuracy.
-    NDArray[np.float64]
         Precision, Recall, TP, FP, FN, F1 Score.
     NDArray[np.float64]
         Interpolated Precision-Recall Curves.
@@ -334,7 +331,6 @@ def compute_precion_recall(
 
     average_precision = np.zeros((n_ious, n_labels), dtype=np.float64)
     average_recall = np.zeros((n_scores, n_labels), dtype=np.float64)
-    accuracy = np.zeros((n_ious, n_scores), dtype=np.float64)
     counts = np.zeros((n_ious, n_scores, n_labels, 6), dtype=np.float64)
 
     pd_labels = data[:, 5].astype(np.int32)
@@ -406,7 +402,6 @@ def compute_precion_recall(
 
             fn_count = gt_count - tp_count
             tp_fp_count = tp_count + fp_count
-            tp_fp_fn_count = tp_fp_count + fn_count
 
             # calculate component metrics
             recall = np.zeros_like(tp_count)
@@ -436,14 +431,6 @@ def compute_precion_recall(
                     f1_score[:, np.newaxis],
                 ),
                 axis=1,
-            )
-
-            # calculate accuracy
-            total_pd_count = label_metadata[:, 1].sum()
-            accuracy[iou_idx, score_idx] = (
-                (tp_count.sum() / tp_fp_fn_count.sum())
-                if total_pd_count > 1e-9
-                else 0.0
             )
 
             # calculate recall for AR
@@ -564,7 +551,6 @@ def compute_precion_recall(
     return (
         ap_results,  # type: ignore[reportReturnType]
         ar_results,
-        accuracy,
         counts,
         pr_curve,
     )
