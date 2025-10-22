af31bf4a233c931f6ebdf8d9915f9899d83d3d07
Charles Zaloom <czaloom4@gmail.com>
2025-10-22 17:08:42 -0400

    new tests

---
+++ tests/common/test_exceptions.py
@@ -0,0 +1,15 @@
+import pytest
+
+import valor_lite.exceptions as exc
+
+
+def test_empty_evaluator_exc():
+    with pytest.raises(exc.EmptyEvaluatorError) as e:
+        raise exc.EmptyEvaluatorError()
+    assert "no data" in str(e)
+
+
+def test_cache_error_exc():
+    with pytest.raises(exc.InternalCacheError) as e:
+        raise exc.InternalCacheError("custom message")
+    assert "custom message" in str(e)
