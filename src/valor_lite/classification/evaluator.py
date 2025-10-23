79ee1ba8d4a10b08bf4476aed3492136faf2e42f
Charles Zaloom <czaloom4@gmail.com>
2025-10-23 10:58:47 -0400

    wip

---
+++ src/valor_lite/classification/evaluator.py
@@ -0,0 +1,247 @@
+import json
+from dataclasses import dataclass
+from pathlib import Path
+
+import numpy as np
+import pyarrow.compute as pc
+import pyarrow.dataset as ds
+from numpy.typing import NDArray
+
+from valor_lite.cache import DataType
+from valor_lite.classification.computation import compute_metrics
+from valor_lite.classification.metric import Metric, MetricType
+from valor_lite.classification.utilities import (
+    unpack_precision_recall_iou_into_metric_lists,
+)
+
+
+@dataclass
+class EvaluatorInfo:
+    number_of_rows: int = 0
+    number_of_datums: int = 0
+    number_of_labels: int = 0
+    number_of_pixels: int = 0
+    number_of_groundtruth_pixels: int = 0
+    number_of_prediction_pixels: int = 0
+    datum_metadata_types: dict[str, DataType] | None = None
+    groundtruth_metadata_types: dict[str, DataType] | None = None
+    prediction_metadata_types: dict[str, DataType] | None = None
+
+
+@dataclass
+class Filter:
+    datums: pc.Expression | None = None
+    groundtruths: pc.Expression | None = None
+    predictions: pc.Expression | None = None
+
+
+class Evaluator:
+    def __init__(
+        self,
+        name: str = "default",
+        directory: str | Path = ".valor",
+        labels_override: dict[int, str] | None = None,
+    ):
+        self._directory = Path(directory)
+        self._name = name
+        self._path = self._directory / name
+        self._cache_path = self._path / "counts"
+        self._metadata_path = self._path / "metadata.json"
+
+        # link cache
+        self._dataset = ds.dataset(self._cache_path, format="parquet")
+
+        # build evaluator meta
+        (
+            self._index_to_label,
+            self._confusion_matrix,
+            self._info,
+        ) = self.generate_meta(self._dataset, labels_override)
+
+        # read config
+        with open(self._metadata_path, "r") as f:
+            types = json.load(f)
+            self._info.datum_metadata_types = types["datum"]
+            self._info.groundtruth_metadata_types = types["groundtruth"]
+            self._info.prediction_metadata_types = types["prediction"]
+        with open(self._cache_path / ".cfg", "r") as f:
+            cfg = json.load(f)
+            self._detailed_batch_size = cfg["batch_size"]
+            self._detailed_rows_per_file = cfg["rows_per_file"]
+            self._detailed_compression = cfg["compression"]
+
+    @property
+    def dataset(self) -> ds.Dataset:
+        return self._dataset
+
+    @property
+    def info(self) -> EvaluatorInfo:
+        return self._info
+
+    @staticmethod
+    def generate_meta(
+        dataset: ds.Dataset,
+        labels_override: dict[int, str] | None,
+    ) -> tuple[dict[int, str], NDArray[np.uint64], EvaluatorInfo]:
+        """
+        Generate cache statistics.
+
+        Parameters
+        ----------
+        dataset : Dataset
+            Valor cache.
+        labels_override : dict[int, str], optional
+            Optional labels override. Use when operating over filtered data.
+
+        Returns
+        -------
+        labels : dict[int, str]
+            Mapping of label ID's to label values.
+        confusion_matrix : NDArray[np.uint64]
+            Array of size (n_labels + 1, n_labels + 1) containing pair counts.
+        info : EvaluatorInfo
+            Evaluator cache details.
+        """
+        labels = labels_override if labels_override else {}
+        info = EvaluatorInfo()
+
+        for fragment in dataset.get_fragments():
+            tbl = fragment.to_table()
+            columns = (
+                "datum_id",
+                "gt_label_id",
+                "pd_label_id",
+                "count",
+            )
+            ids = np.column_stack(
+                [tbl[col].to_numpy() for col in columns]
+            ).astype(np.int64)
+
+            # count number of rows
+            info.number_of_rows += int(tbl.shape[0])
+
+            # count unique datums
+            datum_ids = np.unique(ids[:, 0])
+            info.number_of_datums += int(datum_ids.size)
+
+            # get gt labels
+            gt_label_ids = ids[:, 1]
+            gt_label_ids, gt_indices = np.unique(
+                gt_label_ids, return_index=True
+            )
+            gt_labels = tbl["gt_label"].take(gt_indices).to_pylist()
+            gt_labels = dict(zip(gt_label_ids.astype(int).tolist(), gt_labels))
+            gt_labels.pop(-1, None)
+            labels.update(gt_labels)
+
+            # get pd labels
+            pd_label_ids = ids[:, 2]
+            pd_label_ids, pd_indices, pd_counts = np.unique(
+                pd_label_ids, return_index=True, return_counts=True
+            )
+            pd_labels = tbl["pd_label"].take(pd_indices).to_pylist()
+            pd_labels = dict(zip(pd_label_ids.astype(int).tolist(), pd_labels))
+            pd_labels.pop(-1, None)
+            labels.update(pd_labels)
+
+        # post-process
+        labels.pop(-1, None)
+
+        # create confusion matrix
+        n_labels = len(labels)
+        matrix = np.zeros((n_labels + 1, n_labels + 1), dtype=np.uint64)
+        for fragment in dataset.get_fragments():
+            tbl = fragment.to_table()
+            columns = (
+                "datum_id",
+                "gt_label_id",
+                "pd_label_id",
+            )
+            ids = np.column_stack(
+                [tbl[col].to_numpy() for col in columns]
+            ).astype(np.int64)
+            counts = tbl["count"].to_numpy()
+
+            mask_null_gts = ids[:, 1] == -1
+            mask_null_pds = ids[:, 2] == -1
+            matrix[0, 0] = counts[mask_null_gts & mask_null_pds].sum()
+            for idx in range(n_labels):
+                mask_gts = ids[:, 1] == idx
+                for pidx in range(n_labels):
+                    mask_pds = ids[:, 2] == pidx
+                    matrix[idx + 1, pidx + 1] = counts[
+                        mask_gts & mask_pds
+                    ].sum()
+
+                mask_unmatched_gts = mask_gts & mask_null_pds
+                matrix[idx + 1, 0] = counts[mask_unmatched_gts].sum()
+                mask_unmatched_pds = mask_null_gts & (ids[:, 2] == idx)
+                matrix[0, idx + 1] = counts[mask_unmatched_pds].sum()
+
+        # complete info object
+        info.number_of_labels = len(labels)
+        info.number_of_pixels = matrix.sum()
+        info.number_of_groundtruth_pixels = matrix[1:, :].sum()
+        info.number_of_prediction_pixels = matrix[:, 1:].sum()
+
+        return labels, matrix, info
+
+    def filter(
+        self,
+        filter_expr: Filter,
+        name: str | None = None,
+        directory: str | Path | None = None,
+    ) -> "Evaluator":
+        """
+        Filter evaluator cache.
+
+        Parameters
+        ----------
+        filter_expr : Filter
+            An object containing filter expressions.
+        name : str, optional
+            Filtered cache name.
+        directory : str | Path, optional
+            The directory to store the filtered cache.
+
+        Returns
+        -------
+        Evaluator
+            A new evaluator object containing the filtered cache.
+        """
+        name = name if name else "filtered"
+        directory = directory if directory else self._directory
+        from valor_lite.classification.loader import Loader
+
+        return Loader.filter(
+            name=name,
+            directory=directory,
+            evaluator=self,
+            filter_expr=filter_expr,
+        )
+
+    def compute_precision_recall_iou(self) -> dict[MetricType, list]:
+        """
+        Performs an evaluation and returns metrics.
+
+        Returns
+        -------
+        dict[MetricType, list]
+            A dictionary mapping MetricType enumerations to lists of computed metrics.
+        """
+        results = compute_metrics(counts=self._confusion_matrix)
+        return unpack_precision_recall_iou_into_metric_lists(
+            results=results,
+            index_to_label=self._index_to_label,
+        )
+
+    def evaluate(self) -> dict[MetricType, list[Metric]]:
+        """
+        Computes all available metrics.
+
+        Returns
+        -------
+        dict[MetricType, list[Metric]]
+            Lists of metrics organized by metric type.
+        """
+        return self.compute_precision_recall_iou()
---
+++ src/valor_lite/classification/loader.py
@@ -0,0 +1,365 @@
+import json
+from pathlib import Path
+
+import numpy as np
+import pyarrow as pa
+from tqdm import tqdm
+
+from valor_lite.cache import (
+    CacheWriter,
+    DataType,
+    convert_type_mapping_to_schema,
+)
+from valor_lite.classification.annotation import Classification
+from valor_lite.classification.computation import compute_intermediates
+from valor_lite.classification.evaluator import Evaluator, Filter
+from valor_lite.exceptions import EmptyCacheError
+
+
+class Loader:
+    def __init__(
+        self,
+        directory: str | Path = ".valor",
+        name: str = "default",
+        batch_size: int = 1_000,
+        rows_per_file: int = 10_000,
+        compression: str = "snappy",
+        datum_metadata_types: dict[str, DataType] | None = None,
+        groundtruth_metadata_types: dict[str, DataType] | None = None,
+        prediction_metadata_types: dict[str, DataType] | None = None,
+    ):
+        self._directory = Path(directory)
+        self._name = name
+
+        self._path = self._directory / self._name
+        self._path.mkdir(parents=True, exist_ok=True)
+        self._cache_path = self._path / "counts"
+        self._metadata_path = self._path / "metadata.json"
+
+        self._labels: dict[str, int] = {}
+        self._index_to_label: dict[int, str] = {}
+        self._datum_count = 0
+
+        with open(self._metadata_path, "w") as f:
+            types = {
+                "datum": datum_metadata_types,
+                "groundtruth": groundtruth_metadata_types,
+                "prediction": prediction_metadata_types,
+            }
+            json.dump(types, f, indent=2)
+
+        datum_metadata_schema = convert_type_mapping_to_schema(
+            datum_metadata_types
+        )
+        groundtruth_metadata_schema = convert_type_mapping_to_schema(
+            groundtruth_metadata_types
+        )
+        prediction_metadata_schema = convert_type_mapping_to_schema(
+            prediction_metadata_types
+        )
+
+        self._null_gt_metadata = {
+            s[0]: None for s in groundtruth_metadata_schema
+        }
+        self._null_pd_metadata = {
+            s[0]: None for s in prediction_metadata_schema
+        }
+
+        schema = pa.schema(
+            [
+                ("datum_uid", pa.string()),
+                ("datum_id", pa.int64()),
+                *datum_metadata_schema,
+                # groundtruth
+                ("gt_label", pa.string()),
+                ("gt_label_id", pa.int64()),
+                *groundtruth_metadata_schema,
+                # prediction
+                ("pd_label", pa.string()),
+                ("pd_label_id", pa.int64()),
+                *prediction_metadata_schema,
+                # pair
+                ("count", pa.uint64()),
+            ]
+        )
+        self._cache = CacheWriter(
+            where=self._cache_path,
+            schema=schema,
+            batch_size=batch_size,
+            rows_per_file=rows_per_file,
+            compression=compression,
+        )
+
+    def _add_label(self, value: str) -> int:
+        idx = self._labels.get(value, None)
+        if idx is None:
+            idx = len(self._labels)
+            self._labels[value] = idx
+            self._index_to_label[idx] = value
+        return idx
+
+    def add_data(
+        self,
+        classifications: list[Classification],
+        show_progress: bool = False,
+    ):
+        """
+        Adds classifications to the cache.
+
+        Parameters
+        ----------
+        classifications : list[Classification]
+            A list of Classification objects.
+        show_progress : bool, default=False
+            Toggle for tqdm progress bar.
+        """
+
+        disable_tqdm = not show_progress
+        for classification in tqdm(classifications, disable=disable_tqdm):
+
+            if len(classification.predictions) == 0:
+                raise ValueError(
+                    "Classifications must contain at least one prediction."
+                )
+
+            # update datum uid index
+
+            # cache labels and annotations
+            groundtruth = self._add_label(classification.groundtruth)
+
+            predictions = list()
+            for plabel, pscore in zip(
+                classification.predictions, classification.scores
+            ):
+                label_idx = self._add_label(plabel)
+                predictions.append(
+                    (
+                        label_idx,
+                        pscore,
+                    )
+                )
+
+            pairs = list()
+            scores = np.array([score for _, score in predictions])
+            max_score_idx = np.argmax(scores)
+
+            for idx, (plabel, score) in enumerate(predictions):
+                pairs.append(
+                    (
+                        float(uid_index),
+                        float(groundtruth),
+                        float(plabel),
+                        float(score),
+                        float(max_score_idx == idx),
+                    )
+                )
+
+            if self._detailed_pairs.size == 0:
+                self._detailed_pairs = np.array(pairs)
+            else:
+                self._detailed_pairs = np.concatenate(
+                    [
+                        self._detailed_pairs,
+                        np.array(pairs),
+                    ],
+                    axis=0,
+                )
+
+            # prepare metadata
+            datum_metadata = (
+                segmentation.metadata if segmentation.metadata else {}
+            )
+            gt_metadata = {
+                self._labels[gt.label]: gt.metadata
+                for gt in segmentation.groundtruths
+                if gt.metadata
+            }
+            pd_metadata = {
+                self._labels[pd.label]: pd.metadata
+                for pd in segmentation.predictions
+                if pd.metadata
+            }
+
+            # cache formatting
+            rows = []
+            for idx in range(n_labels):
+                label = self._index_to_label[idx]
+                for pidx in range(n_labels):
+                    # write non-zero intersections to cache
+                    if counts[idx + 1, pidx + 1] > 0:
+                        plabel = self._index_to_label[pidx]
+                        rows.append(
+                            {
+                                # datum
+                                "datum_uid": segmentation.uid,
+                                "datum_id": self._datum_count,
+                                **datum_metadata,
+                                # groundtruth
+                                "gt_label": label,
+                                "gt_label_id": idx,
+                                **gt_metadata.get(idx, {}),
+                                # prediction
+                                "pd_label": plabel,
+                                "pd_label_id": pidx,
+                                **pd_metadata.get(pidx, {}),
+                                # pair
+                                "count": counts[idx + 1, pidx + 1],
+                            }
+                        )
+                # write all unmatched to preserve labels
+                rows.extend(
+                    [
+                        {
+                            # datum
+                            "datum_uid": segmentation.uid,
+                            "datum_id": self._datum_count,
+                            **datum_metadata,
+                            # groundtruth
+                            "gt_label": label,
+                            "gt_label_id": idx,
+                            **gt_metadata.get(idx, {}),
+                            # prediction
+                            "pd_label": None,
+                            "pd_label_id": -1,
+                            # pair
+                            "count": counts[idx + 1, 0],
+                        },
+                        {
+                            # datum
+                            "datum_uid": segmentation.uid,
+                            "datum_id": self._datum_count,
+                            **datum_metadata,
+                            # groundtruth
+                            "gt_label": None,
+                            "gt_label_id": -1,
+                            # prediction
+                            "pd_label": label,
+                            "pd_label_id": idx,
+                            **pd_metadata.get(idx, {}),
+                            # pair
+                            "count": counts[0, idx + 1],
+                        },
+                    ]
+                )
+            rows.append(
+                {
+                    # datum
+                    "datum_uid": segmentation.uid,
+                    "datum_id": self._datum_count,
+                    **datum_metadata,
+                    # groundtruth
+                    "gt_label": None,
+                    "gt_label_id": -1,
+                    # prediction
+                    "pd_label": None,
+                    "pd_label_id": -1,
+                    # pair
+                    "count": counts[0, 0],
+                }
+            )
+            self._cache.write_rows(rows)
+
+            # update datum count
+            self._datum_count += 1
+
+    def finalize(self):
+        """
+        Performs data finalization and some preprocessing steps.
+
+        Returns
+        -------
+        Evaluator
+            A ready-to-use evaluator object.
+        """
+        self._cache.flush()
+        if self._cache.dataset.count_rows() == 0:
+            raise EmptyCacheError()
+
+        return Evaluator(
+            directory=self._directory,
+            name=self._name,
+        )
+
+    @classmethod
+    def filter(
+        cls,
+        directory: str | Path,
+        name: str,
+        evaluator: Evaluator,
+        filter_expr: Filter,
+    ) -> Evaluator:
+        loader = cls(
+            directory=directory,
+            name=name,
+            batch_size=evaluator._detailed_batch_size,
+            rows_per_file=evaluator._detailed_rows_per_file,
+            compression=evaluator._detailed_compression,
+            datum_metadata_types=evaluator.info.datum_metadata_types,
+            groundtruth_metadata_types=evaluator.info.groundtruth_metadata_types,
+            prediction_metadata_types=evaluator.info.prediction_metadata_types,
+        )
+        for fragment in evaluator.dataset.get_fragments():
+            tbl = fragment.to_table(filter=filter_expr.datums)
+
+            columns = (
+                "datum_id",
+                "gt_label_id",
+                "pd_label_id",
+            )
+            pairs = np.column_stack([tbl[col].to_numpy() for col in columns])
+
+            n_pairs = pairs.shape[0]
+            gt_ids = pairs[:, (0, 1)].astype(np.int64)
+            pd_ids = pairs[:, (0, 2)].astype(np.int64)
+
+            if filter_expr.groundtruths is not None:
+                mask_valid_gt = np.zeros(n_pairs, dtype=np.bool_)
+                gt_tbl = tbl.filter(filter_expr.groundtruths)
+                gt_pairs = np.column_stack(
+                    [
+                        gt_tbl[col].to_numpy()
+                        for col in ("datum_id", "gt_label_id")
+                    ]
+                ).astype(np.int64)
+                for gt in np.unique(gt_pairs, axis=0):
+                    mask_valid_gt |= (gt_ids == gt).all(axis=1)
+            else:
+                mask_valid_gt = np.ones(n_pairs, dtype=np.bool_)
+
+            if filter_expr.predictions is not None:
+                mask_valid_pd = np.zeros(n_pairs, dtype=np.bool_)
+                pd_tbl = tbl.filter(filter_expr.predictions)
+                pd_pairs = np.column_stack(
+                    [
+                        pd_tbl[col].to_numpy()
+                        for col in ("datum_id", "pd_label_id")
+                    ]
+                ).astype(np.int64)
+                for pd in np.unique(pd_pairs, axis=0):
+                    mask_valid_pd |= (pd_ids == pd).all(axis=1)
+            else:
+                mask_valid_pd = np.ones(n_pairs, dtype=np.bool_)
+
+            mask_valid = mask_valid_gt | mask_valid_pd
+            mask_valid_gt &= mask_valid
+            mask_valid_pd &= mask_valid
+
+            pairs[~mask_valid_gt, 1] = -1
+            pairs[~mask_valid_pd, 2] = -1
+
+            for idx, col in enumerate(columns):
+                tbl = tbl.set_column(
+                    tbl.schema.names.index(col), col, pa.array(pairs[:, idx])
+                )
+            loader._cache.write_table(tbl)
+
+        loader._cache.flush()
+        if loader._cache.dataset.count_rows() == 0:
+            raise EmptyCacheError()
+
+        evaluator = Evaluator(
+            directory=loader._directory,
+            name=loader._name,
+            labels_override=evaluator._index_to_label,
+        )
+        return evaluator
