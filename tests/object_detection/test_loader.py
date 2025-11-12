92aa583ed3155a705ce64a46a2711e7e61bc605a
Charles Zaloom <czaloom4@gmail.com>
2025-11-12 14:20:55 -0500

    added test

--- tests/object_detection/test_loader.py
+++ tests/object_detection/test_loader.py
@@ -171,3 +171,88 @@ def test_mixed_annotations(
         with pytest.raises(AttributeError) as e:
             loader.add_bitmasks([detection])
         assert "no attribute 'mask'" in str(e)
+
+
+def test_add_data_metadata_handling(loader: Loader):
+    loader.add_bounding_boxes(
+        detections=[
+            Detection(
+                uid="0",
+                metadata={"datum_uid": "a"},
+                groundtruths=[
+                    BoundingBox(
+                        uid="0",
+                        xmin=0,
+                        xmax=10,
+                        ymin=0,
+                        ymax=10,
+                        labels=["dog"],
+                        metadata={"datum_uid": "b"},
+                    )
+                ],
+                predictions=[
+                    BoundingBox(
+                        uid="0",
+                        xmin=0,
+                        xmax=10,
+                        ymin=0,
+                        ymax=10,
+                        labels=["dog"],
+                        scores=[1.0],
+                        metadata={"datum_uid": "c"},
+                    )
+                ],
+            ),
+            Detection(
+                uid="1",
+                metadata={"datum_uid": "a"},
+                groundtruths=[
+                    BoundingBox(
+                        uid="0",
+                        xmin=0,
+                        xmax=10,
+                        ymin=0,
+                        ymax=10,
+                        labels=["dog"],
+                        metadata={"datum_uid": "b"},
+                    )
+                ],
+                predictions=[
+                    BoundingBox(
+                        uid="0",
+                        xmin=0,
+                        xmax=10,
+                        ymin=0,
+                        ymax=10,
+                        labels=["dog"],
+                        scores=[1.0],
+                        metadata={"datum_uid": "c"},
+                    )
+                ],
+            ),
+        ]
+    )
+    loader._detailed_writer.flush()
+    reader = loader._detailed_writer.to_reader()
+
+    datum_uids = set()
+    for tbl in reader.iterate_tables():
+        assert set(tbl.column_names) == {
+            "datum_id",
+            "datum_uid",
+            "gt_id",
+            "gt_label",
+            "gt_label_id",
+            "gt_rect",
+            "gt_uid",
+            "iou",
+            "pd_id",
+            "pd_label",
+            "pd_label_id",
+            "pd_rect",
+            "pd_score",
+            "pd_uid",
+        }
+        for uid in tbl["datum_uid"].to_pylist():
+            datum_uids.add(uid)
+    assert datum_uids == {"0", "1"}
