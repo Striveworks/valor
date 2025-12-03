88c13290b82a39d1f01ab6977264044b288e216f
Charles Zaloom <czaloom4@gmail.com>
2025-12-03 17:06:48 -0500

    wip

--- tests/object_detection/test_regression.py
+++ tests/object_detection/test_regression.py
@@ -1,13 +1,17 @@
+import json
+from pathlib import Path
+
 from valor_lite.object_detection import BoundingBox, Detection, Loader
 
 
-def test_regression_coco_v0_36_6(
+def test_regression_coco_v0_37_3(
     loader: Loader,
-    coco_detections_v0_36_6: list[Detection[BoundingBox]],
-    coco_metrics_v0_36_6: dict[str, list[dict]],
+    coco_metrics_path_v0_37_3: Path,
+    coco_detections_v0_37_3: list[Detection[BoundingBox]],
+    coco_metrics_v0_37_3: dict[str, list[dict]],
 ):
     """Test against Valor v0.36.6"""
-    loader.add_bounding_boxes(coco_detections_v0_36_6)
+    loader.add_bounding_boxes(coco_detections_v0_37_3)
     evaluator = loader.finalize()
     metrics = evaluator.compute_precision_recall(
         iou_thresholds=[0.1],
@@ -18,10 +22,13 @@ def test_regression_coco_v0_36_6(
     }
 
     # verify matching keys
-    assert set(coco_metrics_v0_36_6.keys()) == set(computed_metrics.keys())
+    assert set(coco_metrics_v0_37_3.keys()) == set(computed_metrics.keys())
 
-    for mtype in coco_metrics_v0_36_6.keys():
+    with open(coco_metrics_path_v0_37_3, "w") as f:
+        json.dump(computed_metrics, f, indent=4)
+
+    for mtype in coco_metrics_v0_37_3.keys():
         for m in computed_metrics[mtype]:
-            assert m in coco_metrics_v0_36_6[mtype], mtype
-        for m in coco_metrics_v0_36_6[mtype]:
+            assert m in coco_metrics_v0_37_3[mtype], mtype
+        for m in coco_metrics_v0_37_3[mtype]:
             assert m in computed_metrics[mtype], mtype
