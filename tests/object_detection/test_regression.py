48581178549c88c6d2e899e1fa5a6e056835ff70
Charles Zaloom <czaloom4@gmail.com>
2025-12-04 11:44:22 -0500

    remove print and typo

--- tests/object_detection/test_regression.py
+++ tests/object_detection/test_regression.py
@@ -10,9 +10,7 @@ def test_regression_coco_v0_37_3(
     coco_detections_v0_37_3: list[Detection[BoundingBox]],
     coco_metrics_v0_37_3: dict[str, list[dict]],
 ):
-    """Test against Valor v0.36.6"""
-
-    print(len(coco_detections_v0_37_3))
+    """Test against Valor v0.37.2"""
 
     loader.add_bounding_boxes(coco_detections_v0_37_3)
     evaluator = loader.finalize()
